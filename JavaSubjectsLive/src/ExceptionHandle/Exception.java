4a0d707906f365f61d577bb32c672edd7597f68c
sagardas1 <sagardas.195@gmail.com>
2020-07-18 18:37:46 +0530

    new package added

---
+++ JavaSubjectsLive/src/ExceptionHandle/Exception.java
@@ -0,0 +1,13 @@
+package ExceptionHandle;
+
+public class Exception {
+
+	public void method() {
+
+		try {
+
+		} catch (ArithmeticException | NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+}
