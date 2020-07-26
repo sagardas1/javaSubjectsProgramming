5591015e55640075ca137e39dc33b8aba35b3923
sagardas1 <sagardas.195@gmail.com>
2020-07-26 18:01:52 +0530

    changes

--- JavaSubjectsLive/src/SortingHashMAPValues/MainMethod.java
+++ JavaSubjectsLive/src/SortingHashMAPValues/MainMethod.java
@@ -16,51 +16,49 @@ import Comparable.User;
 public class MainMethod {
 
 	public static void main(String[] args) {
-		
-		HashMap<String ,Student> map=new  HashMap<>();
-		
-		
-		
-		Student e=new Student();
+
+		HashMap<String, Student> map = new HashMap<>();
+
+		Student e = new Student();
 		e.setName("sag");
 		e.setRollNo(27);
-		Student user=new Student();
+		Student user = new Student();
 		user.setName("sq");
 		user.setRollNo(21);
 
-		Student user2=new Student();
+		Student user2 = new Student();
 		user2.setName("sqw");
 		user2.setRollNo(200);
 
 		map.put("sagar", e);
 		map.put("sa", user);
 		map.put("sag", user2);
-		
-		
-		
+
 		sortmap(map);
-		
-	}
-	
-	
-	public static HashMap<String ,Student> sortmap(HashMap<String ,Student> map){
-		List<Map.Entry<String,Student>> list=new ArrayList<Map.Entry<String,Student>>(map.entrySet());
-		
-		Collections.sort(list,new MapValueComparator() );
-		
-	
-HashMap<String , Student> temp=new LinkedHashMap<String, Student>();
 
-for(Map.Entry<String, Student> m:list) {
-temp.put(m.getKey(), m.getValue());
-
-}		
-	Set<Map.Entry<String, Student>> map3=temp.entrySet();
-	for(Map.Entry<String, Student> m8:map3) {
-		System.out.print(m8.getKey()+"    ");
-		System.out.println(((Student)m8.getValue()).getRollNo());
 	}
+
+	public static HashMap<String, Student> sortmap(HashMap<String, Student> map) {
 		
+		List<Map.Entry<String, Student>> sort=new ArrayList<Map.Entry<String,Student>>(map.entrySet());
+		
+		
+		List<Map.Entry<String, Student>> list = new ArrayList<Map.Entry<String, Student>>(map.entrySet());
+
+		Collections.sort(list, new MapValueComparator());
+
+		HashMap<String, Student> temp = new LinkedHashMap<String, Student>();
+
+		for (Map.Entry<String, Student> m : list) {
+			temp.put(m.getKey(), m.getValue());
+
+		}
+		Set<Map.Entry<String, Student>> map3 = temp.entrySet();
+		for (Map.Entry<String, Student> m8 : map3) {
+			System.out.print(m8.getKey() + "    ");
+			System.out.println(((Student) m8.getValue()).getRollNo());
+		}
+
 		return null;
 	}
 }
--- JavaSubjectsLive/src/SortingHashMAPValues/MapValueComparator.java
+++ JavaSubjectsLive/src/SortingHashMAPValues/MapValueComparator.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class MapValueComparator implements Comparator{
+public class MapValueComparator implements Comparator<Map.Entry<String, Student>>{
 
 	
 	@Override
-	public int compare(Object o1, Object o2) {
+	public int compare(Map.Entry<String,Student> o1, Map.Entry<String,Student> o2) {
 		
 		
 		Map.Entry<String, Student> map1=(Entry<String, Student>) o1;
@@ -16,8 +16,8 @@ public class MapValueComparator implements Comparator{
     Student student1=map1.getValue();
     Student student2=map2.getValue();
     
-    if(student1.getRollNo()>student2.getRollNo()) {return 1;}
-    else if(student1.getRollNo()<student2.getRollNo()) {return -1;}else return 0;
+    if(student1.getRollNo()>student2.getRollNo()) {return -1;}
+    else if(student1.getRollNo()<student2.getRollNo()) {return 1;}else return 0;
     
     
 		
