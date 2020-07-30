0afd2a7b963a8b43d21874fb9cf963ee89777878
sagardas1 <sagardas.195@gmail.com>
2020-07-30 21:13:43 +0530

    changes

---
+++ JavaSubjectsLive/src/dynamicLoading/Test2.java
@@ -0,0 +1,21 @@
+package dynamicLoading;
+
+public class Test2 {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+
+		// dynamically load class.
+
+		ClassLoader classLoader = Bike.class.getClassLoader();
+
+		try {
+			Class aClass = classLoader.loadClass("dynamicLoading.Bike");
+			System.out.println("aClass.getName() = " + aClass.getName());
+
+			Bike bike = (Bike) aClass.newInstance();
+			bike.ride();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
