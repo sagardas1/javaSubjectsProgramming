8348acdef280e4a25179bb5495f8647430a4e2f8
sagardas1 <sagardas.195@gmail.com>
2020-07-30 20:33:02 +0530

    changes

--- JavaSubjectsLive/src/overRiding/ChildClass.java
+++ JavaSubjectsLive/src/overRiding/ChildClass.java
@@ -2,7 +2,7 @@ package overRiding;
 
 public class ChildClass extends ParentClass{
 	@Override
-	public void m1() {System.out.println("m1() child class");}
+	protected void m1() {System.out.println("m1() child class");}
 	public void m3() {System.out.println("m3() child class");}
 
 }
--- JavaSubjectsLive/src/overRiding/Main.java
+++ JavaSubjectsLive/src/overRiding/Main.java
@@ -2,6 +2,13 @@ package overRiding;
 
 public class Main {
 	
+	public static void main(String[] args) {
+		
 	
-
+	ChildClass childClass=new ChildClass();
+	childClass.m1();
+	childClass.m2();
+	childClass.m3();
+	
+	}
 }
