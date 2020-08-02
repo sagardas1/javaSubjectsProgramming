4abf5ad23a1895f8e496773e147081e0d679ea1f
sagardas1 <sagardas.195@gmail.com>
2020-08-02 18:10:00 +0530

    create custome annotation.

--- JavaSubjectsLive/src/customeAnnotation/CustomeAnnotation.java
+++ JavaSubjectsLive/src/customeAnnotation/CustomeAnnotation.java
@@ -1,17 +1,37 @@
 package customeAnnotation;
 
+import java.lang.annotation.Annotation;
 
-
-
-@interface smartPhone
-{
+@interface smartPhone {
 	String os() default "ios";
-	double version() default 13.1;
-	
-	}
 
+	double version() default 13.1;
+
+}
 
 @smartPhone
 public class CustomeAnnotation {
+	String brand;
+	int size;
+
+	CustomeAnnotation(String brand, int size) {
+		this.brand = brand;
+		this.size = size;
+	}
+	
+	public static void main(String[] args) {
+		CustomeAnnotation customeAnnotation=new CustomeAnnotation("Nokia", 5);
+		System.out.println(customeAnnotation.brand);
+		
+		Class a=customeAnnotation.getClass();
+		
+		Annotation annotation=a.getAnnotation(smartPhone.class);
+		
+		smartPhone s=(smartPhone)annotation;
+		
+		System.out.println(s.os());
+		
+		
+	}
 
 }
