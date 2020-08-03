7ff56af6faef2553487b0b0abe0eb78eaf2d5e5b
sagardas1 <sagardas.195@gmail.com>
2020-08-03 13:31:32 +0530

    clone

--- JavaSubjectsLive/src/Immutable/Main.java
+++ JavaSubjectsLive/src/Immutable/Main.java
@@ -1,7 +1,6 @@
 package Immutable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
---
+++ JavaSubjectsLive/src/clonning/ObjectCreation.java
@@ -0,0 +1,12 @@
+package clonning;
+
+public class ObjectCreation {
+public static void main(String[] args) throws CloneNotSupportedException {
+	Employee employee=new Employee();
+	employee.setContactInfo("8908273414");
+	employee.setName("sagar das");
+	employee.setRollNo(13);
+	
+Employee employee2=	(Employee) employee.clone();
+}
+}
