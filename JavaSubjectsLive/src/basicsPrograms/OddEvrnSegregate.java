29e9ec663a53c71ed169a3fc70ea8430812ba3a8
sagardas1 <sagardas.195@gmail.com>
2020-08-05 21:25:41 +0530

    changes

---
+++ JavaSubjectsLive/src/basicsPrograms/OddEvrnSegregate.java
@@ -0,0 +1,46 @@
+package basicsPrograms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OddEvrnSegregate {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list= new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		list.add(10);
+		int k=0;
+		int j=list.size()-1;
+		ArrayList<Integer> sortedList= new ArrayList<>(list.size());
+		int a[]= new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			sortedList.add(0);
+			}
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i)%2==0) {
+				a[k]=list.get(i);
+				
+				k+=1;
+			}else {
+				
+				a[j]=list.get(i);
+				j-=1;
+			}
+		}
+				
+			for(int g:a)	{
+				System.out.print(g+"   ");
+			}
+				
+	}
+
+}
---
+++ JavaSubjectsLive/src/basicsPrograms/StringBufferCapacity.java
@@ -0,0 +1,9 @@
+package basicsPrograms;
+
+public class StringBufferCapacity {
+	public static void main(String[] args) {
+		StringBuffer s2 = new StringBuffer();
+		System.out.println(s2.capacity());
+	}
+
+}
