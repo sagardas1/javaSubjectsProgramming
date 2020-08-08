a9cf895bb7c6772623f05627ea45aa3c56fe56fb
sagardas1 <sagardas.195@gmail.com>
2020-08-08 17:36:17 +0530

    changes

---
+++ JavaSubjectsLive/src/overRiddingWithException/Child.java
@@ -0,0 +1,12 @@
+package overRiddingWithException;
+
+import java.io.FileNotFoundException;
+
+public class Child extends Parent{
+	@Override
+	public void m1()throws FileNotFoundException {
+		System.out.println("this is in child class ");
+		
+	}
+
+}
---
+++ JavaSubjectsLive/src/overRiddingWithException/Parent.java
@@ -0,0 +1,11 @@
+package overRiddingWithException;
+
+import java.io.FileNotFoundException;
+
+public class Parent {
+
+	
+	public void m1() throws FileNotFoundException {
+		System.out.println("this method is in Parent class");
+	}
+}
