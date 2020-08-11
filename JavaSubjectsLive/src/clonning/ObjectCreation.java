03a85bbdb28e38606f7db02d9cb62c26c2fcf850
sagardas1 <sagardas.195@gmail.com>
2020-08-12 02:24:09 +0530

    changes

--- JavaSubjectsLive/src/clonning/ObjectCreation.java
+++ JavaSubjectsLive/src/clonning/ObjectCreation.java
@@ -7,6 +7,7 @@ public static void main(String[] args) throws CloneNotSupportedException {
 	employee.setName("sagar das");
 	employee.setRollNo(13);
 	
+@SuppressWarnings("unused")
 Employee employee2=	(Employee) employee.clone();
 }
 }
