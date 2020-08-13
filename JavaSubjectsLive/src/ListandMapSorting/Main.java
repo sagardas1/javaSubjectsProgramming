750966b5a83b2768b4d44c5093a00e6add771880
sagardas1 <sagardas.195@gmail.com>
2020-08-13 16:51:20 +0530

    changes

---
+++ JavaSubjectsLive/src/ListandMapSorting/Main.java
@@ -0,0 +1,33 @@
+package ListandMapSorting;
+
+import java.util.*;
+
+import com.google.gson.Gson;
+
+public class Main {
+	public static void main(String[] args) {
+	Employee employee1=new Employee();
+	employee1.setName("sagar");
+	employee1.setRollNo(29);
+	
+	
+	Employee employee2=new Employee();
+	employee2.setName("Sidharth");
+	employee2.setRollNo(15);
+	
+	HashMap<String, Employee> mao=new HashMap<String, Employee>();
+	mao.put(employee1.getName(),employee1 );
+	mao.put(employee2.getName(),employee2);
+	
+	ArrayList<Map.Entry<String, Employee>> list=new ArrayList<Map.Entry<String,Employee>>(mao.entrySet());
+	Collections.sort(list,new SortByComparatorByroll());
+	
+	
+for(Map.Entry<String, Employee> en:list) {
+	System.out.println(new Gson().toJson(en.getValue()));
+	
+	
+}
+	}
+
+}
---
+++ JavaSubjectsLive/src/ListandMapSorting/SortByComparatorByroll.java
@@ -0,0 +1,28 @@
+package ListandMapSorting;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SortByComparatorByroll implements Comparator<Object> {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		Map.Entry<String, Employee> m1=(Entry<String, Employee>) o1;
+		Map.Entry<String, Employee> m2=(Entry<String, Employee>) o2;
+		
+		Employee e1=m1.getValue();
+		Employee e2=m2.getValue();
+		
+		if(e1.getRollNo()>e2.getRollNo())return 1;
+		else if (e1.getRollNo()<e2.getRollNo()) return -1;
+		else return 0;
+		
+		
+		
+		
+		
+	
+	}
+
+}
