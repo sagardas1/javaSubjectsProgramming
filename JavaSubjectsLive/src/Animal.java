cd4f104aa526b49cd0e9cba4a95829a05f9af528
sagardas1 <sagardas.195@gmail.com>
2020-08-13 20:56:21 +0530

    changes

---
+++ JavaSubjectsLive/src/Animal.java
@@ -0,0 +1,14 @@
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		Animal animal=null;
+		if(animal==null) {
+			System.out.println("errror");
+		}
+		if(animal.equals(null)) {
+			System.out.println("hsbhsg");
+		}
+	}
+
+}
---
+++ JavaSubjectsLive/src/nullCheckInObject/Animal.java
@@ -0,0 +1,15 @@
+package nullCheckInObject;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		Animal animal=null;
+		if(animal==null) {
+			System.out.println("errror");
+		}
+		if(animal.equals(null)) {
+			System.out.println("hsbhsg");
+		}
+	}
+
+}
