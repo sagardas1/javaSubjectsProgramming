f5099d797ea9fcae839efd93a361e641d85b5b68
sagardas1 <sagardas.195@gmail.com>
2020-08-14 14:55:50 +0530

    supper and this implementation

---
+++ JavaSubjectsLive/src/thisandSuper/Super1.java
@@ -0,0 +1,16 @@
+package thisandSuper;
+
+public class Super1 {
+	
+	int a;
+	int b;
+	public Super1() {
+		super();
+		this.a = 10;
+		this.b = 10;
+	}
+	public void getnum() {
+		System.out.println(a+"   "+b);
+	}
+
+}
---
+++ JavaSubjectsLive/src/thisandSuper/ThisAndSuper.java
@@ -0,0 +1,18 @@
+package thisandSuper;
+
+public class ThisAndSuper extends Super1{
+	int a;
+	
+	int b;
+
+	public ThisAndSuper(int a, int b) {
+		super();
+		this.a = 20;
+		this.b = 20;
+	}
+
+	public static void main(String[] args) {
+		ThisAndSuper andSuper=new ThisAndSuper(10, 20);
+		System.out.println(andSuper.a);
+	}
+}
