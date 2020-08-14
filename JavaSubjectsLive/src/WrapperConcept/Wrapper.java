c22a29c02115a2acd595e1db37f0cad3e0a23ff9
sagardas1 <sagardas.195@gmail.com>
2020-08-14 13:44:18 +0530

    changes

--- JavaSubjectsLive/src/WrapperConcept/Wrapper.java
+++ JavaSubjectsLive/src/WrapperConcept/Wrapper.java
@@ -2,10 +2,21 @@ package WrapperConcept;
 
 public class Wrapper {
 	public static void main(String[] args) {
-		Integer a=100;
-		Integer b=100;
+		Integer a = 150;
+		Integer b = 150;
+
+		if (a == b) {
+			System.out.println("equals");
+		} else {
+			System.out.println("not");
+		}
 		
-		if(a==b) {System.out.println("equals");}else {System.out.println("not");}
+		
+		if (a.equals(b)) {
+			System.out.println("equals");
+		} else {
+			System.out.println("not");
+		}
 	}
 
 }
