eb71fdf00cb281813884889a1de4210f186ab4d6
sagardas1 <sagardas.195@gmail.com>
2020-08-14 19:58:39 +0530

    changes

--- JavaSubjectsLive/src/genericImpli/MainClass.java
+++ JavaSubjectsLive/src/genericImpli/MainClass.java
@@ -1,6 +1,8 @@
 package genericImpli;
 
-public class MainClass {
+import com.google.gson.Gson;
+
+public class MainClass<T, V> {
 	public static void main(String[] args) {
 
 		Base<Employee> b1 = new Base<Employee>();
@@ -9,10 +11,7 @@ public class MainClass {
 		employee.setRollNo(13);
 		b1.setT(employee);
 		b1.setCount(1);
-		
-		
-		
-		
+
 		Base<Address> b2 = new Base<Address>();
 		Address address = new Address();
 		address.setCity("banglore");
@@ -20,7 +19,25 @@ public class MainClass {
 		address.setPincode(123);
 		b2.setT(address);
 		b2.setCount(1);
+		MainClass<Address, Employee> a = new MainClass<Address, Employee>();
+
+		Employee employee2 = new Employee();
+		employee2.setName("hhh");
+		employee2.setRollNo(11);
+		@SuppressWarnings("unused")
+		Address address2 = a.m1(employee2);
 		
-		
+		System.out.println(new Gson().toJson(address2));
+
+	}
+
+	// generic methods
+	@SuppressWarnings("unchecked")
+	public T m1(V v) {
+		Address address = new Address();
+address.setCity("sai");
+address.setCityId(12);
+address.setPincode(123);
+		return (T) address;
 	}
 }
