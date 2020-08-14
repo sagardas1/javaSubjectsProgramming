d05f8394fd4fbb07cc2263a13a0fb26efd5bbd8f
sagardas1 <sagardas.195@gmail.com>
2020-08-14 13:46:08 +0530

    changes

---
+++ JavaSubjectsLive/src/MAin.java
@@ -0,0 +1,20 @@
+
+public class MAin {
+	
+	public static void main(String[] args) {
+		m1(null);
+		
+	}
+
+	private static void m1(Object object) {
+	System.out.println("object");
+		
+	}
+	private static void m1(String object) {
+		System.out.println("string");
+		
+	}
+	
+	
+
+}
