f8a4481c585273cbdee288d0ab7229b82562664b
sagardas1 <sagardas.195@gmail.com>
2020-08-14 13:42:56 +0530

    WrapperClass

---
+++ JavaSubjectsLive/src/WrapperConcept/Wrapper.java
@@ -0,0 +1,11 @@
+package WrapperConcept;
+
+public class Wrapper {
+	public static void main(String[] args) {
+		Integer a=100;
+		Integer b=100;
+		
+		if(a==b) {System.out.println("equals");}else {System.out.println("not");}
+	}
+
+}
