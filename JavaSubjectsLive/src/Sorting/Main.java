89d8cf115233cca05455ab133d05cafb2d243a28
sagardas1 <sagardas.195@gmail.com>
2020-08-17 21:14:18 +0530

    chnages

--- JavaSubjectsLive/src/Sorting/Main.java
+++ JavaSubjectsLive/src/Sorting/Main.java
@@ -1,13 +1,12 @@
 package Sorting;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static int main(String args[]) {
-List a=new ArrayList<>();
+List<Object> a=new ArrayList<>();
 a.add(1);
 a.add(2);
 return 0;
