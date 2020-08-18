f64e724be02aa14b55065b4778c08719a57a7942
sagardas1 <sagardas.195@gmail.com>
2020-08-18 12:38:41 +0530

    changaes

---
+++ JavaSubjectsLive/src/superClass/Child.java
@@ -0,0 +1,24 @@
+package superClass;
+
+public class Child extends ParentClass{
+
+	int a;
+	int b;
+	
+	Child(int a, int b) {
+		super(100, 200);
+		this.a=a;
+		
+		this.b=b;
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+		Child child=new Child(11, 12);
+		System.out.println(child.a);
+		child.m1();
+	}
+
+}
---
+++ JavaSubjectsLive/src/superClass/ParentClass.java
@@ -0,0 +1,18 @@
+package superClass;
+
+public class ParentClass {
+	int a;
+	int b;
+	
+	ParentClass(int a,int b){
+		this.a=a;
+		this.b=b;
+		
+		
+	}
+	
+	public void m1() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
