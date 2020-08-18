9affe69d6f1502bb28cbdff13081ef694aa46a06
sagardas1 <sagardas.195@gmail.com>
2020-08-18 12:29:45 +0530

    new implimentation for this

---
+++ JavaSubjectsLive/src/UseOfThisandSuper.java
@@ -0,0 +1,16 @@
+
+public class UseOfThisandSuper {
+int a;
+int b;
+UseOfThisandSuper(int a,int b){
+	this.a=a;
+	this.b=b;
+	
+	
+}
+
+public static void main(String[] args) {
+	UseOfThisandSuper ofThisandSuper=new UseOfThisandSuper(10, 29);
+	System.out.println(ofThisandSuper.a);
+}
+}
