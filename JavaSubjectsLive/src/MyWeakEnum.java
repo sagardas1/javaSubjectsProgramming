0fc8623b563669254559b4aace2bdcccdb9fe6ac
sagardas1 <sagardas.195@gmail.com>
2020-08-25 12:47:17 +0530

    changes

--- JavaSubjectsLive/src/MyWeakEnum.java
+++ JavaSubjectsLive/src/MyWeakEnum.java
@@ -1,16 +1,16 @@
 
 public enum MyWeakEnum {
-	
-	MONDAY(1),TUESDAY(2),WEDNESDAY(3),THURSDAY(4),FRIDAY(5),SATURDAY(6),SUNDAY(7);
+
+	MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(7);
 	final int i;
-	
-	MyWeakEnum(int i){
-	this.i=i;	
-		
+
+	MyWeakEnum(int i) {
+		this.i = i;
+
 	}
-	
+
 	public int getNum() {
 		return this.i;
 	}
-	
+
 }
