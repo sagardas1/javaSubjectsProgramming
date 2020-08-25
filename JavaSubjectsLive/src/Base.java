09d9e8e2abdf25dd9309c36611e5548e40e69d08
sagardas1 <sagardas.195@gmail.com>
2020-08-25 12:45:48 +0530

    chnages

--- JavaSubjectsLive/src/Base.java
+++ JavaSubjectsLive/src/Base.java
@@ -1,33 +1,19 @@
-class Derived  
-{ 
-    public void getDetails(String s) 
-    { 
-        System.out.println("Derived class "+ s); 
-    } 
-} 
-  
+class Derived {
+	public void getDetails(String s) {
+		System.out.println("Derived class " + s);
+	}
+}
 
+public class Base extends Derived {
 
+	public void getDetails(String s) {
+		System.out.println("Base class " + s);
+	}
 
+	public static void main(String[] args) {
+		Derived derived = new Base();
 
-public class Base extends Derived 
-{ 
-
-    public void getDetails(String s) 
-    { 
-        System.out.println("Base class " + s); 
-    } 
-
-
-
-
-
-
-    public static void main(String[] args) 
-    { 
-    	Derived derived=new Base();
-    	
-        Base obj = (Base) derived;
-        obj.getDetails("TEST"); 
-    } 
+		Base obj = (Base) derived;
+		obj.getDetails("TEST");
+	}
 }
