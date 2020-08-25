dae313bf03773fdf69a216a291a7797253b32013
sagardas1 <sagardas.195@gmail.com>
2020-08-25 12:45:17 +0530

    changes

--- JavaSubjectsLive/src/FirstNonRepeted.java
+++ JavaSubjectsLive/src/FirstNonRepeted.java
@@ -1,29 +1,28 @@
 
 public class FirstNonRepeted {
 	public static void main(String[] args) {
-		
-		String s="saggagggsdfdagkgfgq";
-		
-@SuppressWarnings("unused")
-char[] arr=s.toCharArray();
 
-@SuppressWarnings("unused")
-int count=0;
-int size=s.length();
-while(size!=0) {
-char a=	s.charAt(0);
-s=s.replace(a+"", "");
+		String s = "saggagggsdfdagkgfgq";
 
-if(size-s.length()==1) {
-	System.out.println(a);
-	break;
-}
-else {
-	size=s.length();
-	
-}
-	
-}
+		@SuppressWarnings("unused")
+		char[] arr = s.toCharArray();
+
+		@SuppressWarnings("unused")
+		int count = 0;
+		int size = s.length();
+		while (size != 0) {
+			char a = s.charAt(0);
+			s = s.replace(a + "", "");
+
+			if (size - s.length() == 1) {
+				System.out.println(a);
+				break;
+			} else {
+				size = s.length();
+
+			}
+
+		}
 	}
 
 }
