bbd4eeefa78bc4ad5b978e5febb3d3040bd8aa08
sagardas1 <sagardas.195@gmail.com>
2020-08-25 12:45:30 +0530

    changes

--- JavaSubjectsLive/src/Example.java
+++ JavaSubjectsLive/src/Example.java
@@ -1,19 +1,19 @@
 
 public class Example {
-	
+
 	public static void m1() throws MyExceptionProvide {
-		int b=0;
-		if(b==0) {
-		
-	throw new MyExceptionProvide("b cant be zero");
+		int b = 0;
+		if (b == 0) {
+
+			throw new MyExceptionProvide("b cant be zero");
 		}
 	}
-	
-public static void main(String[] args) {
-	try {
-		m1();
-	} catch (MyExceptionProvide e) {
-		e.getMessage1();
+
+	public static void main(String[] args) {
+		try {
+			m1();
+		} catch (MyExceptionProvide e) {
+			e.getMessage1();
+		}
 	}
-}
 }
