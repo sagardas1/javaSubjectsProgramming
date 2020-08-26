1d9046acbc11d5571c65650fcd6fc70082d5f8f4
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:30:41 +0530

    changes

--- JavaSubjectsLive/src/overRiding/Main.java
+++ JavaSubjectsLive/src/overRiding/Main.java
@@ -1,14 +1,23 @@
 package overRiding;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import com.google.gson.internal.StringMap;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		
+	Runnable r=()->System.out.println("HI");
+new Thread(r).start();
 	
-	ChildClass childClass=new ChildClass();
-	childClass.m1();
-	childClass.m2();
-	childClass.m3();
+	
+	
 	
 	}
+	
+	
+	
+	
+	
+	
 }
