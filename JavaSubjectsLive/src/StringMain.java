82cf8cbca00357db1cbe27dbd8778749280f3fab
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:33:26 +0530

    changews

--- JavaSubjectsLive/src/StringMain.java
+++ JavaSubjectsLive/src/StringMain.java
@@ -1,15 +1,15 @@
 
 public class StringMain {
-	
+
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
-		String s1="abcdec";
-		String s2="ecabcd";
-		
-		
-		s1+=s2;
-		if(true) {}
-		
+		String s1 = "abcdec";
+		String s2 = "ecabcd";
+
+		s1 += s2;
+		if (true) {
+		}
+
 	}
 
 }
