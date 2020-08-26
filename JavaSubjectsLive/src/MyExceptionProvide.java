be74b0731cee18d3b5500ed613435f3d9281a7e2
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:31:53 +0530

    chnages

--- JavaSubjectsLive/src/MyExceptionProvide.java
+++ JavaSubjectsLive/src/MyExceptionProvide.java
@@ -1,16 +1,15 @@
 
 @SuppressWarnings("serial")
-public class MyExceptionProvide extends Exception{
-	
+public class MyExceptionProvide extends Exception {
+
 	String name;
-	MyExceptionProvide(String name){
-		this.name=name;
+
+	MyExceptionProvide(String name) {
+		this.name = name;
 	}
-	
-	
+
 	public void getMessage1() {
-		System.out.println("Exception occure in: "+name);
+		System.out.println("Exception occure in: " + name);
 	}
-	
 
 }
