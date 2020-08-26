d57ebe29e93715241b15bce377986ebe62a25894
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:32:11 +0530

    changes

--- JavaSubjectsLive/src/RemoveAllWhiteSpace.java
+++ JavaSubjectsLive/src/RemoveAllWhiteSpace.java
@@ -1,20 +1,20 @@
 
 public class RemoveAllWhiteSpace {
-public static void main(String[] args) {
-	String a="i am a boy";
-	
-	a=a.replace(" ", "");
-	System.out.println(a);
-	
-	//or
-	String newString="";
-	
-for(char k:a.toCharArray()) {
-	if(k!=' ' ) {
-		newString+=k+"";
-	}
-}
+	public static void main(String[] args) {
+		String a = "i am a boy";
 
-System.out.println(newString);
-}
+		a = a.replace(" ", "");
+		System.out.println(a);
+
+		// or
+		String newString = "";
+
+		for (char k : a.toCharArray()) {
+			if (k != ' ') {
+				newString += k + "";
+			}
+		}
+
+		System.out.println(newString);
+	}
 }
