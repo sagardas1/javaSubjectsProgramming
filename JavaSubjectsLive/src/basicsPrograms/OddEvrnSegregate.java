c185c3eccfaf5bbdc99661183626e6903dec8952
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:34:26 +0530

    changes

--- JavaSubjectsLive/src/basicsPrograms/OddEvrnSegregate.java
+++ JavaSubjectsLive/src/basicsPrograms/OddEvrnSegregate.java
@@ -3,10 +3,10 @@ package basicsPrograms;
 import java.util.ArrayList;
 
 public class OddEvrnSegregate {
-	
+
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> list= new ArrayList<>();
+
+		ArrayList<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(2);
 		list.add(3);
@@ -17,29 +17,29 @@ public class OddEvrnSegregate {
 		list.add(8);
 		list.add(9);
 		list.add(10);
-		int k=0;
-		int j=list.size()-1;
-		ArrayList<Integer> sortedList= new ArrayList<>(list.size());
-		int a[]= new int[list.size()];
+		int k = 0;
+		int j = list.size() - 1;
+		ArrayList<Integer> sortedList = new ArrayList<>(list.size());
+		int a[] = new int[list.size()];
 		for (int i = 0; i < list.size(); i++) {
 			sortedList.add(0);
-			}
-		for(int i=0;i<list.size();i++) {
-			if(list.get(i)%2==0) {
-				a[k]=list.get(i);
-				
-				k+=1;
-			}else {
-				
-				a[j]=list.get(i);
-				j-=1;
+		}
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) % 2 == 0) {
+				a[k] = list.get(i);
+
+				k += 1;
+			} else {
+
+				a[j] = list.get(i);
+				j -= 1;
 			}
 		}
-				
-			for(int g:a)	{
-				System.out.print(g+"   ");
-			}
-				
+
+		for (int g : a) {
+			System.out.print(g + "   ");
+		}
+
 	}
 
 }
