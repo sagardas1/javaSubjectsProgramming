99f589a798bd1bd9bdad6b6f6b6b9facc93b0cd6
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:33:44 +0530

    chnages

--- JavaSubjectsLive/src/ValueComaprator.java
+++ JavaSubjectsLive/src/ValueComaprator.java
@@ -2,20 +2,20 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class ValueComaprator implements Comparator<Map.Entry<Integer, Integer>>{
+public class ValueComaprator implements Comparator<Map.Entry<Integer, Integer>> {
 
 	@Override
 	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-		// TODO Auto-generated method stub
-		int a=o1.getValue();
-		int b=o2.getValue();
-		
-		if(a>b) return 1;
-		else if(a<b) return -1;
-		else return 0;
-		
-	}
+		int a = o1.getValue();
+		int b = o2.getValue();
 
-	
+		if (a > b)
+			return 1;
+		else if (a < b)
+			return -1;
+		else
+			return 0;
+
+	}
 
 }
