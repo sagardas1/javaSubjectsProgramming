2778a9ee3597b74fda1efe8e63b5421d33db9fb5
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:35:12 +0530

    changes

--- JavaSubjectsLive/src/bst/MybinarySearchTree.java
+++ JavaSubjectsLive/src/bst/MybinarySearchTree.java
@@ -1,40 +1,40 @@
 package bst;
 
 public class MybinarySearchTree {
-	
-	class Node{
+
+	class Node {
 		int key;
-		Node left,right;
-		Node(int key){
-			this.key=key;
-			left=right=null;
+		Node left, right;
+
+		Node(int key) {
+			this.key = key;
+			left = right = null;
 		}
-		
-	}
-	
-	Node root;
-	MybinarySearchTree(){
-		root=null;
-	}
-	
-	void insert(int key) {
-		root=insertRec( root,key);
+
 	}
 
-	private Node insertRec(Node root,int key) {
-		if(root==null) {
-			root=new Node(key);
+	Node root;
+
+	MybinarySearchTree() {
+		root = null;
+	}
+
+	void insert(int key) {
+		root = insertRec(root, key);
+	}
+
+	private Node insertRec(Node root, int key) {
+		if (root == null) {
+			root = new Node(key);
 			return root;
-			
+
 		}
-		if(key < root.key ) {
-			root.left=insertRec(root.left,key);
-		}
-		else if(key>root.key) {
-			root.right=insertRec(root.right,key);
+		if (key < root.key) {
+			root.left = insertRec(root.left, key);
+		} else if (key > root.key) {
+			root.right = insertRec(root.right, key);
 		}
 		return root;
 	}
-
 
 }
