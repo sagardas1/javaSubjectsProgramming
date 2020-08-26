13d00b89094894d75cb1beedcc3f44ad62e1f40f
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:35:45 +0530

    changes

--- JavaSubjectsLive/src/clonning/Employee.java
+++ JavaSubjectsLive/src/clonning/Employee.java
@@ -1,34 +1,38 @@
 package clonning;
 
+public class Employee implements Cloneable {
 
-
-public class Employee implements Cloneable{
-	
 	private String name;
 	private int rollNo;
 	private String contactInfo;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getRollNo() {
 		return rollNo;
 	}
+
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
+
 	public String getContactInfo() {
 		return contactInfo;
 	}
+
 	public void setContactInfo(String contactInfo) {
 		this.contactInfo = contactInfo;
 	}
-	
+
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		// TODO Auto-generated method stub
 		return super.clone();
 	}
-	}
+}
