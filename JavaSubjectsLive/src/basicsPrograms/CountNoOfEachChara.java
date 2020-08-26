c07fbdff35541db84d5032ef2285b65dffd86b82
sagardas1 <sagardas.195@gmail.com>
2020-08-26 12:34:09 +0530

    changes

--- JavaSubjectsLive/src/basicsPrograms/CountNoOfEachChara.java
+++ JavaSubjectsLive/src/basicsPrograms/CountNoOfEachChara.java
@@ -3,28 +3,29 @@ package basicsPrograms;
 import java.util.*;
 
 public class CountNoOfEachChara {
-	
+
 	public static void main(String[] args) {
-		int count=0;
-		String a="sagar";
-		
-		Map<Character,Integer>  m=new HashMap<Character,Integer>();
-		char[]arr=a.toCharArray();
-		
-		for(char i:arr){
-			if(m.containsKey(i)) {
-				 count=m.get(i);
-				m.put(i, count+1);
-				count=0;
-			}else {m.put(i, 1);}
+		int count = 0;
+		String a = "sagar";
+
+		Map<Character, Integer> m = new HashMap<Character, Integer>();
+		char[] arr = a.toCharArray();
+
+		for (char i : arr) {
+			if (m.containsKey(i)) {
+				count = m.get(i);
+				m.put(i, count + 1);
+				count = 0;
+			} else {
+				m.put(i, 1);
+			}
 		}
-		
-		Set<Map.Entry<Character, Integer>> entry=m.entrySet();
-		
-		for(Map.Entry<Character, Integer> en:entry) {
-			System.out.println(en.getKey()+" "+en.getValue());
+
+		Set<Map.Entry<Character, Integer>> entry = m.entrySet();
+
+		for (Map.Entry<Character, Integer> en : entry) {
+			System.out.println(en.getKey() + " " + en.getValue());
 		}
 	}
-	
 
 }
