d930fd53f5b376f8f451fb9fff3c8feebc96aec0
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:41:27 +0530

    changes

--- JavaSubjectsLive/src/collection/Student.java
+++ JavaSubjectsLive/src/collection/Student.java
@@ -1,26 +1,32 @@
 package collection;
 
 public class Student {
-private String name;
-private int rollNo;
-private String address;
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public int getRollNo() {
-	return rollNo;
-}
-public void setRollNo(int rollNo) {
-	this.rollNo = rollNo;
-}
-public String getAddress() {
-	return address;
-}
-public void setAddress(String address) {
-	this.address = address;
-}
+	private String name;
+	private int rollNo;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
