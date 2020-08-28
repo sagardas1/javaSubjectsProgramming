857353f488cfd120203f2234f8c71cdcdd3ce20b
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:41:49 +0530

    changes

--- JavaSubjectsLive/src/collection/TestForFetching.java
+++ JavaSubjectsLive/src/collection/TestForFetching.java
@@ -5,36 +5,34 @@ import java.util.List;
 
 public class TestForFetching {
 
-	public List<Student> fetch(){
-		Student std=new Student();
+	public List<Student> fetch() {
+		Student std = new Student();
 		std.setAddress("xxx");
 		std.setName("Sagar");
 		std.setRollNo(10);
 		return null;
 	}
+
 	public static void main(String[] args) {
 //		String name="hello world";
 //		System.out.println(name.substring(1,2));
 //String[] nameList=name.split("o");
 //for(String a:nameList) {System.out.print (a +"   ");}
 
-		String textFile="I am a good boy i am  very  good  at code code";
-		String wordList[]=textFile.split(" ");
-	 
+		String textFile = "I am a good boy i am  very  good  at code code";
+		String wordList[] = textFile.split(" ");
 
-		List<String> a= Arrays.asList(wordList); 
-		int size=a.size();
-		while(size!=0) {
-			String word=a.get(0);
+		List<String> a = Arrays.asList(wordList);
+		int size = a.size();
+		while (size != 0) {
+			String word = a.get(0);
 			a.remove(word);
-			if(size-a.size()>1) {
+			if (size - a.size() > 1) {
 				System.out.println(word);
 			}
-			size=a.size();
-			
-			
+			size = a.size();
+
 		}
-		
-		
+
 	}
 }
