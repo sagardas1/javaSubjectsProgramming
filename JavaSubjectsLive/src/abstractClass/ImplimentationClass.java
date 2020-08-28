a5af4cd86c4c5451bb5b1047396deb0d854cb3ff
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:38:09 +0530

    changes

--- JavaSubjectsLive/src/abstractClass/ImplimentationClass.java
+++ JavaSubjectsLive/src/abstractClass/ImplimentationClass.java
@@ -1,11 +1,11 @@
 package abstractClass;
 
-public class ImplimentationClass extends MyabstractClass{
+public class ImplimentationClass extends MyabstractClass {
 
 	@Override
 	public void getName() {
 		System.out.println("Sagar");
-		
+
 	}
 
 }
