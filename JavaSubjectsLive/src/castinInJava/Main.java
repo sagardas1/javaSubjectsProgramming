43554acebb0295c9f470cb9d84c25cbcc7975ab5
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:41:08 +0530

    changes

--- JavaSubjectsLive/src/castinInJava/Main.java
+++ JavaSubjectsLive/src/castinInJava/Main.java
@@ -2,15 +2,14 @@ package castinInJava;
 
 public class Main {
 	public static void main(String[] args) {
-		
-	@SuppressWarnings("unused")
-	A aa=new B();
-		
-	//B bb=(B) aa;
-		
-		
+
+		@SuppressWarnings("unused")
+		A aa = new B();
+
+		// B bb=(B) aa;
+
 //	aa.m3();
-		
+
 	}
 
 }
