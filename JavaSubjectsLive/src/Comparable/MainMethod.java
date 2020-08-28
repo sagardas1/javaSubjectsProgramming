14486e2e7dae8f83626bca41983e4da5e23d6aad
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:42:10 +0530

    changes

--- JavaSubjectsLive/src/Comparable/MainMethod.java
+++ JavaSubjectsLive/src/Comparable/MainMethod.java
@@ -7,28 +7,28 @@ import java.util.List;
 public class MainMethod {
 
 	public static void main(String[] args) {
-		
-		List<User> set=new ArrayList<>();
-		User e=new User();
+
+		List<User> set = new ArrayList<>();
+		User e = new User();
 		e.setName("sag");
 		e.setRollNo(27);
-		User user=new User();
+		User user = new User();
 		user.setName("sq");
 		user.setRollNo(21);
 
-		User user2=new User();
+		User user2 = new User();
 		user2.setName("sqw");
 		user2.setRollNo(200);
 
 		set.add(e);
 		set.add(user2);
 		set.add(user);
-		
+
 		Collections.sort(set);
-		
-		for(User s:set) {
+
+		for (User s : set) {
 			System.out.println(s.getRollNo());
 		}
 	}
-	
+
 }
