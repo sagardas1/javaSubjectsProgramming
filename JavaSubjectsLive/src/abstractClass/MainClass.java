e90bb122b5aae9bad0207698743f4d19cdb8a717
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:39:38 +0530

    chnages

--- JavaSubjectsLive/src/abstractClass/MainClass.java
+++ JavaSubjectsLive/src/abstractClass/MainClass.java
@@ -3,11 +3,11 @@ package abstractClass;
 public class MainClass {
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		ImplimentationClass class1=new ImplimentationClass();
+		ImplimentationClass class1 = new ImplimentationClass();
 		System.out.println(class1.getPassword());
 		class1.getName();
 		class1.m1();
-		//class1.m2();
+		// class1.m2();
 	}
 
 }
