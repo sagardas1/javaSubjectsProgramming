265aa3dacf5539b73f8e7fdef861aa12707730c1
sagardas1 <sagardas.195@gmail.com>
2020-08-28 12:38:25 +0530

    changes

--- JavaSubjectsLive/src/abstractClass/MyabstractClass.java
+++ JavaSubjectsLive/src/abstractClass/MyabstractClass.java
@@ -1,19 +1,19 @@
 package abstractClass;
 
 public abstract class MyabstractClass {
-	
+
 	public abstract void getName();
-	
-	public  String getPassword() {
+
+	public String getPassword() {
 		return "Sagardas@12345";
 	}
-	
-public static void m1() {
-	System.out.println("sagar");
-}
 
-@SuppressWarnings("unused")
-private void m2() {
-	System.out.println("private");
-}
+	public static void m1() {
+		System.out.println("sagar");
+	}
+
+	@SuppressWarnings("unused")
+	private void m2() {
+		System.out.println("private");
+	}
 }
