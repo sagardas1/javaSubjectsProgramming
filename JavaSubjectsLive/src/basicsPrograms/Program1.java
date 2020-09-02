a9c5f3d5fb2ca44307048bcf45c4a7809a14aeff
sagardas1 <sagardas.195@gmail.com>
2020-09-02 14:36:33 +0530

    chnages

--- JavaSubjectsLive/src/basicsPrograms/Program1.java
+++ JavaSubjectsLive/src/basicsPrograms/Program1.java
@@ -1,16 +1,17 @@
 package basicsPrograms;
 
 public class Program1 {
-	public static int i=10;
+	public static int i = 10;
+
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		
-		Program1 program1=new Program1();
-		program1.i=i+1;
-		
-		Program1 program2=new Program1();
-		program2.i=i+1;
-		
+
+		Program1 program1 = new Program1();
+		program1.i = i + 1;
+
+		Program1 program2 = new Program1();
+		program2.i = i + 1;
+
 		System.out.println(program1.i);
 		System.out.println(program2.i);
 	}
