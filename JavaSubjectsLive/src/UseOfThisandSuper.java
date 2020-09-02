ddaa18375454fc36674c1416efe02b6d28b06f9f
sagardas1 <sagardas.195@gmail.com>
2020-09-02 14:35:35 +0530

    chnages

--- JavaSubjectsLive/src/UseOfThisandSuper.java
+++ JavaSubjectsLive/src/UseOfThisandSuper.java
@@ -1,16 +1,16 @@
 
 public class UseOfThisandSuper {
-int a;
-int b;
-UseOfThisandSuper(int a,int b){
-	this.a=a;
-	this.b=b;
-	
-	
-}
+	int a;
+	int b;
 
-public static void main(String[] args) {
-	UseOfThisandSuper ofThisandSuper=new UseOfThisandSuper(10, 29);
-	System.out.println(ofThisandSuper.a);
-}
+	UseOfThisandSuper(int a, int b) {
+		this.a = a;
+		this.b = b;
+
+	}
+
+	public static void main(String[] args) {
+		UseOfThisandSuper ofThisandSuper = new UseOfThisandSuper(10, 29);
+		System.out.println(ofThisandSuper.a);
+	}
 }
