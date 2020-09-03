c6e9e4308ff89ab0ebd29d959188e2218d62d5fe
sagardas1 <sagardas.195@gmail.com>
2020-09-03 12:25:19 +0530

    changes

--- JavaSubjectsLive/src/reflextion/Student.java
+++ JavaSubjectsLive/src/reflextion/Student.java
@@ -1,11 +1,14 @@
 package reflextion;
 
 public class Student {
-	
+
 	private String name;
 	private int area;
-	
-	Student(String name,int area){this.name=name;this.area=area;}
+
+	Student(String name, int area) {
+		this.name = name;
+		this.area = area;
+	}
 
 	public String getName() {
 		return name;
@@ -14,15 +17,14 @@ public class Student {
 	public int getArea() {
 		return area;
 	}
-	
-	
+
 	@SuppressWarnings("unused")
-	private String method1(String name,int area) {
-		String a="";
-		
-		System.out.println("in this private method. Name is "+name+ " and "+area);
-		return a="in this private method. Name is "+name+ " and "+area;
-		
+	private String method1(String name, int area) {
+		String a = "";
+
+		System.out.println("in this private method. Name is " + name + " and " + area);
+		return a = "in this private method. Name is " + name + " and " + area;
+
 	}
 
 	@SuppressWarnings("unused")
