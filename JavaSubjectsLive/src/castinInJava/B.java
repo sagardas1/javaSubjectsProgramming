0a7a9feeba82c951f6b878801ef7c586bfadb4ea
sagardas1 <sagardas.195@gmail.com>
2020-09-04 14:20:58 +0530

    changes

--- JavaSubjectsLive/src/castinInJava/B.java
+++ JavaSubjectsLive/src/castinInJava/B.java
@@ -1,11 +1,11 @@
 package castinInJava;
 
-public class B extends A{
+public class B extends A {
 
 	public void m1() {
 		System.out.println(" b m1");
 	}
-	
+
 	public void m3() {
 		System.out.println("m3");
 	}
