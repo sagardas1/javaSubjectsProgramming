77b9445b8f3f2854021ff9eface9f3bc80094bfc
sagardas1 <sagardas.195@gmail.com>
2020-09-04 14:01:45 +0530

    changes

---
+++ JavaSubjectsLive/src/bst1/BSTApp.java
@@ -0,0 +1,68 @@
+package bst1;
+
+import castinInJava.Main;
+
+class Node{
+	Node right;
+	Node left;
+	int data;
+	
+	
+}
+public class BSTApp {
+	public static void main(String[] args) {
+		BST bst=new  BST();
+		Node root=null;
+		root=bst.insert(root,10);
+		root=bst.insert(root,1);
+		root=bst.insert(root,2);
+		root=bst.insert(root,6);
+		
+		root=bst.insert(root,4);
+		root=bst.insert(root,4);
+		
+		bst.inOrder(root);
+		
+	}
+	
+	
+}
+
+class BST{
+
+	public Node insert(Node root, int i) {
+		if(root==null) {
+			return createNewNode(i);
+			
+		}
+		if(i<root.data) {
+			root.left=insert(root.left,i);
+		}
+		else if(i>=root.data) {
+			root.right=insert(root.right, i);
+		}
+		return root;
+	}
+
+	public void inOrder(Node root) {
+		if(root==null) {
+			return;
+			
+		}
+		
+		inOrder(root.left);
+		System.out.println(root.data);
+		inOrder(root.right);
+		
+	}
+
+	private Node createNewNode(int i) {
+	Node n=new Node();
+	n.right=null;
+	n.left=null;
+	n.data=i;
+		return n;
+	}
+	
+	
+}
