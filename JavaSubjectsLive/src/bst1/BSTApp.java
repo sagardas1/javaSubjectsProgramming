fe7cb14a39d9982ef195659815626fda1c04b437
sagardas1 <sagardas.195@gmail.com>
2020-09-04 14:16:08 +0530

    changes

--- JavaSubjectsLive/src/bst1/BSTApp.java
+++ JavaSubjectsLive/src/bst1/BSTApp.java
@@ -1,78 +1,75 @@
 package bst1;
 
-class Node{
+class Node {
 	Node right;
 	Node left;
 	int data;
-	
-	
+
 }
+
 public class BSTApp {
 	public static void main(String[] args) {
-		BST bst=new  BST();
-		Node root=null;
-		root=bst.insert(root,10);
-		root=bst.insert(root,1);
-		root=bst.insert(root,2);
-		root=bst.insert(root,6);
-		
-		root=bst.insert(root,4);
-		root=bst.insert(root,4);
-		
+		BST bst = new BST();
+		Node root = null;
+		root = bst.insert(root, 10);
+		root = bst.insert(root, 1);
+		root = bst.insert(root, 2);
+		root = bst.insert(root, 6);
+
+		root = bst.insert(root, 4);
+		root = bst.insert(root, 4);
+
 		bst.inOrder(root);
-		
+
 		bst.preOrder(root);
-		
+
 	}
-	
-	
+
 }
 
-class BST{
+class BST {
 
 	public Node insert(Node root, int i) {
-		if(root==null) {
+		if (root == null) {
 			return createNewNode(i);
-			
+
 		}
-		if(i<root.data) {
-			root.left=insert(root.left,i);
-		}
-		else if(i>=root.data) {
-			root.right=insert(root.right, i);
+		if (i < root.data) {
+			root.left = insert(root.left, i);
+		} else if (i >= root.data) {
+			root.right = insert(root.right, i);
 		}
 		return root;
 	}
 
 	public void preOrder(Node root) {
-	if(root==null) {
-		return;
-		
-	}
-	System.out.print (root.data+" ");
-	preOrder(root.left);
-	preOrder(root.right);
+		if (root == null) {
+			return;
+
+		}
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
 	}
 
 	public void inOrder(Node root) {
-		if(root==null) {
+		if (root == null) {
 			return;
-			
+
 		}
-		
+
 		inOrder(root.left);
 		System.out.println(root.data);
 		inOrder(root.right);
-		
+
 	}
 
 	private Node createNewNode(int i) {
-	Node n=new Node();
-	n.right=null;
-	n.left=null;
-	n.data=i;
+		Node n = new Node();
+		n.right = null;
+		n.left = null;
+		n.data = i;
 		return n;
 	}
-	
-	
+
 }
