2b0babed5c886fda9699cfefe3989d4700cebc6f
sagardas1 <sagardas.195@gmail.com>
2020-09-06 18:12:41 +0530

    changes

--- JavaSubjectsLive/src/java8Features/MyMethodReference.java
+++ JavaSubjectsLive/src/java8Features/MyMethodReference.java
@@ -1,34 +1,27 @@
 package java8Features;
 
 public class MyMethodReference {
-	
+
 	public static void main(String[] args) {
-		
-		
-		MyMethodReferanceInterface in=A::method;
+
+		MyMethodReferanceInterface in = A::method;
 		in.say();
-		
-		A a=new A();
-		MyMethodReferanceInterface i=a::method1;
-		
-		
-		
+
+		A a = new A();
+		MyMethodReferanceInterface i = a::method1;
+
 	}
-	
 
 }
 
+class A {
 
-class A{
-	
-	
-	public static void method(){
+	public static void method() {
 		System.out.println("method reference");
 	}
-	
-	public  void method1(){
+
+	public void method1() {
 		System.out.println("method reference");
 	}
-	
-	
+
 }
