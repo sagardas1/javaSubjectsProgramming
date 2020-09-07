b5ca68fd2a4fe31eb52c5549fcad2e425cd4b999
sagardas1 <sagardas.195@gmail.com>
2020-09-07 17:59:49 +0530

    CHNAGES

--- JavaSubjectsLive/src/SortingHashMAPValues/MapValueComparator.java
+++ JavaSubjectsLive/src/SortingHashMAPValues/MapValueComparator.java
@@ -4,26 +4,26 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class MapValueComparator implements Comparator<Object>{
+public class MapValueComparator implements Comparator<Object> {
 
 	@Override
 	public int compare(Object o1, Object o2) {
 
 		@SuppressWarnings("unchecked")
-		Map.Entry<String, Student> m1=(Entry<String, Student>) o1;
+		Map.Entry<String, Student> m1 = (Entry<String, Student>) o1;
 		@SuppressWarnings("unchecked")
-		Map.Entry<String, Student> m2=(Entry<String, Student>) o2;
-		
-		Student s1=m1.getValue();
-		Student s2=m2.getValue();
-		
-		if(s1.getRollNo()>s2.getRollNo()) {return 1;}else if(s1.getRollNo()<s2.getRollNo()) {return -1;} else return 0;
-		
-		
-		
-	}
+		Map.Entry<String, Student> m2 = (Entry<String, Student>) o2;
 
-	
-	
+		Student s1 = m1.getValue();
+		Student s2 = m2.getValue();
+
+		if (s1.getRollNo() > s2.getRollNo()) {
+			return 1;
+		} else if (s1.getRollNo() < s2.getRollNo()) {
+			return -1;
+		} else
+			return 0;
+
+	}
 
 }
