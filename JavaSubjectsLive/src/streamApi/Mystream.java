b73c3931745c5f098fb9c49bab69973ca30d7848
sagardas1 <sagardas.195@gmail.com>
2020-09-07 18:01:33 +0530

    changes

--- JavaSubjectsLive/src/streamApi/Mystream.java
+++ JavaSubjectsLive/src/streamApi/Mystream.java
@@ -7,33 +7,29 @@ import java.util.Map;
 import java.util.Set;
 
 public class Mystream {
-public static void main(String[] args) {
-	Map<EmployeeCase,Integer> map=new HashMap<>();
-	
-	EmployeeCase case1=new EmployeeCase();
-	case1.setName("sagar das");
-	case1.setRedge(12);
-	
-	EmployeeCase case2=new EmployeeCase();
-	case2.setName("Sidharth Das");
-	case2.setRedge(11);
-	
-	map.put(case1, 1);
-	map.put(case2, 2);
-	
-	@SuppressWarnings("unused")
-	Set<Map.Entry<EmployeeCase, Integer>> entrySet=map.entrySet();
-	
-	List<EmployeeCase> list=new ArrayList<>();
-	
-	list.add(case1);
-	list.add(case2);
-	
-	
-	//list.stream().filter(()->)
-	
-	
-	
-	
-}
+	public static void main(String[] args) {
+		Map<EmployeeCase, Integer> map = new HashMap<>();
+
+		EmployeeCase case1 = new EmployeeCase();
+		case1.setName("sagar das");
+		case1.setRedge(12);
+
+		EmployeeCase case2 = new EmployeeCase();
+		case2.setName("Sidharth Das");
+		case2.setRedge(11);
+
+		map.put(case1, 1);
+		map.put(case2, 2);
+
+		@SuppressWarnings("unused")
+		Set<Map.Entry<EmployeeCase, Integer>> entrySet = map.entrySet();
+
+		List<EmployeeCase> list = new ArrayList<>();
+
+		list.add(case1);
+		list.add(case2);
+
+		// list.stream().filter(()->)
+
+	}
 }
