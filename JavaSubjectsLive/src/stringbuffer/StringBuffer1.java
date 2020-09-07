6b3f5ddc1c369f8b33f61c6105c367ecad49dfb1
sagardas1 <sagardas.195@gmail.com>
2020-09-07 18:01:54 +0530

    chaanges

--- JavaSubjectsLive/src/stringbuffer/StringBuffer1.java
+++ JavaSubjectsLive/src/stringbuffer/StringBuffer1.java
@@ -1,20 +1,18 @@
 package stringbuffer;
 
 public class StringBuffer1 {
-public static void main(String[] args) {
-	StringBuffer sb=new StringBuffer("Sagar");
-	sb.append("Sagar");
-	System.out.println(sb.capacity());
-	System.out.println(sb.length());
-	//sb="sagar";
-	
-	
-	
-	StringBuilder builder=new StringBuilder();
-	builder.append("sagar");
-	
-	System.out.println(builder.capacity());
-	System.out.println(builder.length());
-	
-}
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Sagar");
+		sb.append("Sagar");
+		System.out.println(sb.capacity());
+		System.out.println(sb.length());
+		// sb="sagar";
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("sagar");
+
+		System.out.println(builder.capacity());
+		System.out.println(builder.length());
+
+	}
 }
