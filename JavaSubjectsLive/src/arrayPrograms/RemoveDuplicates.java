2c949bf1a4f2ffdbfb3e752485481da11a50fa38
sagardas1 <sagardas.195@gmail.com>
2020-09-07 17:56:54 +0530

    chnages

--- JavaSubjectsLive/src/arrayPrograms/RemoveDuplicates.java
+++ JavaSubjectsLive/src/arrayPrograms/RemoveDuplicates.java
@@ -5,16 +5,15 @@ import java.util.*;
 import com.google.gson.Gson;
 
 public class RemoveDuplicates {
-public static void main(String[] args) {
-	int a[]= {1,2,4,5,2};
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 4, 5, 2 };
 
-	Set se=new HashSet<Integer>();
-	
-	for(int i=0;i<a.length;i++) {
-		se.add(a[i]);
+		Set se = new HashSet<Integer>();
+
+		for (int i = 0; i < a.length; i++) {
+			se.add(a[i]);
+		}
+
+		System.out.println(new Gson().toJson(se));
 	}
-	
-	
-	System.out.println(new Gson().toJson(se));
-}
 }
