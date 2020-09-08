17995e5695d8c699c356991adf026db9e94ba665
sagardas1 <sagardas.195@gmail.com>
2020-09-08 22:46:36 +0530

    changes

--- JavaSubjectsLive/src/overRiding/ParentClass.java
+++ JavaSubjectsLive/src/overRiding/ParentClass.java
@@ -1,11 +1,16 @@
 package overRiding;
 
 public class ParentClass {
-	
-	ParentClass(){
+
+	ParentClass() {
 		System.out.println("parentgsdvfhvsh");
 	}
-	
-	void m1() {System.out.println("m1() parent Class");}
-	void m2() {System.out.println("m2() parent Class");}
+
+	void m1() {
+		System.out.println("m1() parent Class");
+	}
+
+	void m2() {
+		System.out.println("m2() parent Class");
+	}
 }
