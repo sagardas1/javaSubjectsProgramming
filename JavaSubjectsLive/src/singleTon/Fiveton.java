a90500d4b382bcf6cb1047e2133a7347c68672b9
sagardas1 <sagardas.195@gmail.com>
2020-09-08 22:45:40 +0530

    chnages

--- JavaSubjectsLive/src/singleTon/Fiveton.java
+++ JavaSubjectsLive/src/singleTon/Fiveton.java
@@ -3,28 +3,28 @@ package singleTon;
 import java.util.ArrayList;
 
 public class Fiveton {
-	public  static Fiveton fiveton=null;
-static int count=0;
-	static ArrayList<Fiveton> list=new ArrayList<>();
+	public static Fiveton fiveton = null;
+	static int count = 0;
+	static ArrayList<Fiveton> list = new ArrayList<>();
 
 	private Fiveton() {
-		
+
 	}
-	
-public static Fiveton getInstance() {
-	if(fiveton==null) {
-		fiveton=new Fiveton();
-		list.add(fiveton);
-		count+=1;
-	}else if(count<5) {
-		
-		fiveton=new Fiveton();
-		list.add(fiveton);
-		count+=1;
-	}else {
-		fiveton=list.get(count%5);
-		count+=1;
+
+	public static Fiveton getInstance() {
+		if (fiveton == null) {
+			fiveton = new Fiveton();
+			list.add(fiveton);
+			count += 1;
+		} else if (count < 5) {
+
+			fiveton = new Fiveton();
+			list.add(fiveton);
+			count += 1;
+		} else {
+			fiveton = list.get(count % 5);
+			count += 1;
+		}
+		return fiveton;
 	}
-	 return fiveton;
-}
 }
