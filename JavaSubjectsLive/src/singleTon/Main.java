aff1e9400ab12ea22a00e5a0ea3803fc0a33895b
sagardas1 <sagardas.195@gmail.com>
2020-09-08 22:45:14 +0530

    chnages

--- JavaSubjectsLive/src/singleTon/Main.java
+++ JavaSubjectsLive/src/singleTon/Main.java
@@ -2,18 +2,16 @@ package singleTon;
 
 public class Main {
 	public static void main(String[] args) {
-		
-	
-	Fiveton a =Fiveton.getInstance();
-	Fiveton b=Fiveton.getInstance();
-	Fiveton c=Fiveton.getInstance();
-	Fiveton d=Fiveton.getInstance();
 
-	
-	System.out.println(a.hashCode());
-	System.out.println(b.hashCode());
-	System.out.println(c.hashCode());
-	System.out.println(d.hashCode());
+		Fiveton a = Fiveton.getInstance();
+		Fiveton b = Fiveton.getInstance();
+		Fiveton c = Fiveton.getInstance();
+		Fiveton d = Fiveton.getInstance();
+
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		System.out.println(c.hashCode());
+		System.out.println(d.hashCode());
 	}
-	
+
 }
