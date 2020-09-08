85627d5b7051629333235bf7d409c3864ba89ef3
sagardas1 <sagardas.195@gmail.com>
2020-09-08 22:46:04 +0530

    chnages

--- JavaSubjectsLive/src/overRiding/ChildClass.java
+++ JavaSubjectsLive/src/overRiding/ChildClass.java
@@ -1,13 +1,18 @@
 package overRiding;
 
-public class ChildClass extends ParentClass{
-	
-	ChildClass(){
+public class ChildClass extends ParentClass {
+
+	ChildClass() {
 		System.out.println("dhsvfg");
 	}
-	
+
 	@Override
-	protected void m1() {System.out.println("m1() child class");}
-	public void m3() {System.out.println("m3() child class");}
+	protected void m1() {
+		System.out.println("m1() child class");
+	}
+
+	public void m3() {
+		System.out.println("m3() child class");
+	}
 
 }
