b78a67033a4fa6bd86a7bcabf75530776569f227
sagardas1 <sagardas.195@gmail.com>
2020-09-09 19:06:04 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/UniqueSubString.java
+++ JavaSubjectsLive/src/arrayProgram/UniqueSubString.java
@@ -3,22 +3,20 @@ package arrayProgram;
 import java.util.ArrayList;
 
 public class UniqueSubString {
-	
+
 	public static void main(String[] args) {
-		String a="abdd";
-		ArrayList<String> list=new ArrayList<String>();
-		for(int i=0;i<a.length();i++) {
-			if(i!=a.length()-1) {
-				list.add(a.charAt(i)+""+a.charAt(i+1)+"");
-			
-			}
-			else 
-			list.add(a.charAt(i)+""+a.charAt(0)+"");
+		String a = "abdd";
+		ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < a.length(); i++) {
+			if (i != a.length() - 1) {
+				list.add(a.charAt(i) + "" + a.charAt(i + 1) + "");
+
+			} else
+				list.add(a.charAt(i) + "" + a.charAt(0) + "");
 		}
-		
-		
-		for(String s:list) {
-			
+
+		for (String s : list) {
+
 		}
 	}
 
