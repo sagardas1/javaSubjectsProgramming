2ad1d1c693943a96d6ad57ebfb2fa97fe1af2d7f
sagardas1 <sagardas.195@gmail.com>
2020-09-09 19:05:23 +0530

    changes

--- JavaSubjectsLive/src/streamApi/Main.java
+++ JavaSubjectsLive/src/streamApi/Main.java
@@ -7,31 +7,28 @@ import java.util.stream.Collectors;
 import com.google.gson.Gson;
 
 public class Main {
-public static void main(String[] args) {
-	EmployeeCase case1=new EmployeeCase("dak",1);
-	EmployeeCase case2=new EmployeeCase("Alpha",23);
-	EmployeeCase case3=new EmployeeCase("void",4);
-	EmployeeCase case5=new EmployeeCase("tronic",55);
-	EmployeeCase case4=new EmployeeCase("syan",66);
-	EmployeeCase case6=new EmployeeCase("aman",77);
-	EmployeeCase case7=new EmployeeCase("piya",88);
-	EmployeeCase case8=new EmployeeCase("dak",2);
-	
-	List<EmployeeCase> list=new ArrayList<EmployeeCase>();
-	list.add(case7);
-	list.add(case6);
-	list.add(case4);
-	list.add(case5);
-	list.add(case3);
-	list.add(case2);
-	list.add(case1);
-	list.add(case8);
-	
-	
-	
-	List<EmployeeCase> list1=	 (List<EmployeeCase>) list.stream().filter(p ->(p.getName().equals("dak")))
-;
-	
-	System.out.println(new Gson().toJson(list1));
-}
+	public static void main(String[] args) {
+		EmployeeCase case1 = new EmployeeCase("dak", 1);
+		EmployeeCase case2 = new EmployeeCase("Alpha", 23);
+		EmployeeCase case3 = new EmployeeCase("void", 4);
+		EmployeeCase case5 = new EmployeeCase("tronic", 55);
+		EmployeeCase case4 = new EmployeeCase("syan", 66);
+		EmployeeCase case6 = new EmployeeCase("aman", 77);
+		EmployeeCase case7 = new EmployeeCase("piya", 88);
+		EmployeeCase case8 = new EmployeeCase("dak", 2);
+
+		List<EmployeeCase> list = new ArrayList<EmployeeCase>();
+		list.add(case7);
+		list.add(case6);
+		list.add(case4);
+		list.add(case5);
+		list.add(case3);
+		list.add(case2);
+		list.add(case1);
+		list.add(case8);
+
+		List<EmployeeCase> list1 = (List<EmployeeCase>) list.stream().filter(p -> (p.getName().equals("dak")));
+
+		System.out.println(new Gson().toJson(list1));
+	}
 }
