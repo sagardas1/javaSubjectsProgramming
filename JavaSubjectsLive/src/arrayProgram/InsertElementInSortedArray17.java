505b6c3b06febe7cc056d8fa6b04ce9dec9c66ed
sagardas1 <sagardas.195@gmail.com>
2020-09-09 19:07:30 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/InsertElementInSortedArray17.java
+++ JavaSubjectsLive/src/arrayProgram/InsertElementInSortedArray17.java
@@ -1,25 +1,25 @@
 package arrayProgram;
 
 public class InsertElementInSortedArray17 {
-public static void main(String[] args) {
-	int a[]={ 12, 16, 20, 40, 50, 70 };
-	int b=58;
-	int finalArray[]=new int[a.length+1];
-	int c=0;
-	for(int i=0;i<a.length;i++) {
-		if(b<a[i]) {
-			finalArray[c]=b;
-			c+=1;
-			finalArray[c]=a[i];
-			c+=1;
-		}else {
-			finalArray[c]=a[i];
-			c+=1;
+	public static void main(String[] args) {
+		int a[] = { 12, 16, 20, 40, 50, 70 };
+		int b = 58;
+		int finalArray[] = new int[a.length + 1];
+		int c = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (b < a[i]) {
+				finalArray[c] = b;
+				c += 1;
+				finalArray[c] = a[i];
+				c += 1;
+			} else {
+				finalArray[c] = a[i];
+				c += 1;
+			}
+		}
+
+		for (int i : finalArray) {
+			System.out.print(i + "  ");
 		}
 	}
-	
-	for(int i:finalArray) {
-		System.out.print(i+"  ");
-	}
-}
 }
