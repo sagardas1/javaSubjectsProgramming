09402a64c504f1a8fdd3fb250f911b9c3fe06a4a
sagardas1 <sagardas.195@gmail.com>
2020-09-09 19:05:47 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/Factorisation.java
+++ JavaSubjectsLive/src/arrayProgram/Factorisation.java
@@ -2,15 +2,14 @@ package arrayProgram;
 
 public class Factorisation {
 	public static void main(String[] args) {
-		
-	
-	int a=24;
-	int f=2;
-	while (a!=0) {
-		if(a%2==0) {
-			
-		}
-	}
 
-}
+		int a = 24;
+		int f = 2;
+		while (a != 0) {
+			if (a % 2 == 0) {
+
+			}
+		}
+
+	}
 }
