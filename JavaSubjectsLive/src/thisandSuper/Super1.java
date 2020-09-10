60f151494ef60d0f07a47849bbb962c502673cb2
sagardas1 <sagardas.195@gmail.com>
2020-09-10 14:29:20 +0530

    changes

--- JavaSubjectsLive/src/thisandSuper/Super1.java
+++ JavaSubjectsLive/src/thisandSuper/Super1.java
@@ -1,16 +1,18 @@
 package thisandSuper;
 
 public class Super1 {
-	
+
 	int a;
 	int b;
+
 	public Super1() {
 		super();
 		this.a = 10;
 		this.b = 10;
 	}
+
 	public void getnum() {
-		System.out.println(a+"   "+b);
+		System.out.println(a + "   " + b);
 	}
 
 }
