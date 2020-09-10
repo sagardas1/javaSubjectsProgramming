58f97201de0b8c4b6a0b9b9e9fab48f5fa9456ba
sagardas1 <sagardas.195@gmail.com>
2020-09-10 14:28:56 +0530

    changes

--- JavaSubjectsLive/src/superClass/ParentClass.java
+++ JavaSubjectsLive/src/superClass/ParentClass.java
@@ -3,14 +3,13 @@ package superClass;
 public class ParentClass {
 	int a;
 	int b;
-	
-	ParentClass(int a,int b){
-		this.a=a;
-		this.b=b;
-		
-		
+
+	ParentClass(int a, int b) {
+		this.a = a;
+		this.b = b;
+
 	}
-	
+
 	public void m1() {
 		System.out.println(a);
 		System.out.println(b);
