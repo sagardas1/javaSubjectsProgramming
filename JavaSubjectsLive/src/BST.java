547a80c681a9da6187d105b4f3f7d493da414782
sagardas1 <sagardas.195@gmail.com>
2020-09-10 22:39:54 +0530

    chnages

--- JavaSubjectsLive/src/BST.java
+++ JavaSubjectsLive/src/BST.java
@@ -1,23 +1,23 @@
-class Node{
-	
+class Node {
+
 	Node left;
 	Node right;
 	int data;
-	
+
 }
 
 public class BST {
 	public static void main(String[] args) {
-		BinarySearch search=new BinarySearch();
-		Node root=null;
-		root=search.insert(root,10);
-		root=search.insert(root,2);
-		root=search.insert(root,4);
-		root=search.insert(root,3);
-		root=search.insert(root,7);
-		
+		BinarySearch search = new BinarySearch();
+		Node root = null;
+		root = search.insert(root, 10);
+		root = search.insert(root, 2);
+		root = search.insert(root, 4);
+		root = search.insert(root, 3);
+		root = search.insert(root, 7);
+
 		search.inOrder(root);
-		
+
 	}
 
 }
@@ -25,36 +25,35 @@ public class BST {
 class BinarySearch {
 
 	public Node insert(Node root, int i) {
-		if(root==null) {
+		if (root == null) {
 			return createNewNode(i);
 		}
-		if(i<root.data) {
-			root.left=insert(root.left,i);
-		}else if(i>root.data) {
-			root.right=insert(root.right, i);
+		if (i < root.data) {
+			root.left = insert(root.left, i);
+		} else if (i > root.data) {
+			root.right = insert(root.right, i);
 		}
 		return root;
 	}
 
 	public void inOrder(Node root) {
-		if(root==null) {
+		if (root == null) {
 			return;
 		}
-		
+
 		inOrder(root.left);
 		System.out.println(root.data);
 		inOrder(root.right);
-		
+
 	}
 
 	private Node createNewNode(int i) {
-		Node n=new Node();
-		n.left=null;
-		n.right=null;
-		n.data=10;
-		
+		Node n = new Node();
+		n.left = null;
+		n.right = null;
+		n.data = 10;
+
 		return n;
 	}
 
-	
 }
