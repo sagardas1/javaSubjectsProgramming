b17d3b36860ac74dcab475383177d151bb4d1a47
sagardas1 <sagardas.195@gmail.com>
2020-09-11 16:36:04 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/FindOutCircular26.java
+++ JavaSubjectsLive/src/arrayProgram/FindOutCircular26.java
@@ -1,27 +1,29 @@
 package arrayProgram;
 
-public class FindOutCircular26{
+public class FindOutCircular26 {
 	public static void main(String[] args) {
-		int a[] = { 1,  0 };
-int count=0;
+		int a[] = { 1, 0 };
+		int count = 0;
 		for (int i = 0; i < a.length; i++) {
 			if (i == 0) {
-			} else if(i!=a.length-1){
+			} else if (i != a.length - 1) {
 				if (i + 1 == a[i]) {
-					continue;}else {
-						count+=1;
-						System.out.println("not");
-						break;}
-			}else  {
-				if(i==a[0])
 					continue;
-				
+				} else {
+					count += 1;
+					System.out.println("not");
+					break;
+				}
+			} else {
+				if (i == a[0])
+					continue;
+
 			}
 		}
-		
-		if(count==0) {
+
+		if (count == 0) {
 			System.out.println(a.length);
-		}else
+		} else
 			System.out.println("-1");
 	}
 }
