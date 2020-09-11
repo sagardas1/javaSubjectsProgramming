9009affff7a299178ce94fd76d2e9130bf835d5b
sagardas1 <sagardas.195@gmail.com>
2020-09-11 16:34:50 +0530

    chnage

---
+++ JavaSubjectsLive/src/FindLargestAndSmallestInUnsorted.java
@@ -0,0 +1,22 @@
+
+public class FindLargestAndSmallestInUnsorted {
+	public static void main(String[] args) {
+		int a[] = { 1, 3, 4, 2, 5, 6, 4, 2, 1 };
+
+		int smallest = 0;
+		int largest = 0;
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+			System.out.println(smallest);
+			if (a[i] < smallest) {
+				smallest = a[i];
+			}
+			if (largest < a[i]) {
+				largest = a[i];
+			}
+		}
+
+		System.out.println("smallest number is - " + smallest);
+		System.out.println("largest number is - " + largest);
+	}
+}
---
+++ JavaSubjectsLive/src/LongestSubString.java
@@ -0,0 +1,33 @@
+
+public class LongestSubString {
+	public static void main(String[] args) {
+		String a = "sagardas";
+
+		String temp = "";
+		String longest = "";
+		int j = 0;
+		for (int i = 0; i < a.length(); i++) {
+			j = i;
+			temp = "";
+			if (!temp.contains(a.charAt(i) + "")) {
+
+				while (!temp.contains(a.charAt(j) + "") && j < a.length() - 1) {
+
+					temp += a.charAt(j) + "";
+
+					j += 1;
+
+				}
+
+				if (temp.length() >= longest.length())
+					longest = temp;
+
+			} else {
+				j = 0;
+				temp = "";
+
+			}
+		}
+		System.out.println(longest);
+	}
+}
