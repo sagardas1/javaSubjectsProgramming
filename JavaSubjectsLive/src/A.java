bda715f9f4cfce35f9b429ebf03986e0084239ee
sagardas1 <sagardas.195@gmail.com>
2020-09-11 18:35:52 +0530

    changes

--- JavaSubjectsLive/src/A.java
+++ JavaSubjectsLive/src/A.java
@@ -1,20 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+
 public class A {
 
-static class Helper{
-	private int data =5;
-	public void bump(int inc) 
-	{inc++;
-	data=data+inc;}
-}
-	
+	static class Helper {
+		private int data = 5;
+
+		public void bump(int inc) {
+			inc++;
+			data = data + inc;
+		}
+	}
+
 	public static void main(String[] args) {
 
-		Helper helper=new Helper();
-		int data=2;
-		helper.bump(data);
-		System.out.println(helper.data);
+		int a[] = { 1, 2, 3, 2 };
 		
 		
+		//List<Integer> li = Arrays.stream(a).collect(supplier, accumulator, combiner)
+				
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 56);
+		System.out.println(new Gson().toJson(list));
 	}
 
 }
