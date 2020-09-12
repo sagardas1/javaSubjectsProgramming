ded930b52a6d31e62a9eb16c302336c03c311da0
sagardas1 <sagardas.195@gmail.com>
2020-09-12 22:40:55 +0530

    changes

--- JavaSubjectsLive/src/overRiding/ChildClass.java
+++ JavaSubjectsLive/src/overRiding/ChildClass.java
@@ -5,20 +5,19 @@ import java.nio.file.FileAlreadyExistsException;
 import inheritance.Animal;
 import inheritance.Dog;
 
-public class ChildClass  {
-	
-	
+public class ChildClass {
+
 	public static void main(String[] args) {
-		String x="a";
-		//Runnable r=()->System.out.println(x);
-		x="b";
-		//r.run();
-		
+		String x = "a";
+		// Runnable r=()->System.out.println(x);
+		x = "b";
+		// r.run();
+
 		"b".equals(x);
-		
-		int d=20;
-	String a=	d + "".toString();
-	
+
+		int d = 20;
+		String a = d + "".toString();
+
 	}
 
 }
