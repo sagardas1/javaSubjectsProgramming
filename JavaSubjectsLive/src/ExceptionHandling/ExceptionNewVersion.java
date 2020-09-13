ae9f372dc49a78834879d3535e7bb9d07fe2e37e
sagardas1 <sagardas.195@gmail.com>
2020-09-13 19:09:00 +0530

    changes

--- JavaSubjectsLive/src/ExceptionHandling/ExceptionNewVersion.java
+++ JavaSubjectsLive/src/ExceptionHandling/ExceptionNewVersion.java
@@ -5,21 +5,18 @@ import java.io.FileReader;
 
 public class ExceptionNewVersion {
 
-	
 	public static void main(String[] args) {
-		try(BufferedReader bufferedReader=new BufferedReader(new FileReader("file.txt")))
-		{
-			
-		}catch(Exception e) {
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader("file.txt"))) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
 	public void m1() {
-		try{
-			
-		}catch(Exception e) {}
+		try {
+
+		} catch (Exception e) {
+		}
 	}
 }
