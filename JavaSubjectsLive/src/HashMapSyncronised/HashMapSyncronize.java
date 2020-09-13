734fd583468c85fb3531b887c9c22b36113da9c4
sagardas1 <sagardas.195@gmail.com>
2020-09-13 19:09:29 +0530

    changes

--- JavaSubjectsLive/src/HashMapSyncronised/HashMapSyncronize.java
+++ JavaSubjectsLive/src/HashMapSyncronised/HashMapSyncronize.java
@@ -6,21 +6,19 @@ import java.util.Map;
 import java.util.Set;
 
 public class HashMapSyncronize {
-public static void main(String[] args) {
-	
-	HashMap<String ,Integer> map=new HashMap<String, Integer>();
-	map.put("sagar", 1);
-	map.put("sidharth", 2);
-	
-	Map<String, Integer> m=Collections.synchronizedMap(map);
-	
-	Set<Map.Entry<String, Integer>> e=m.entrySet();
-	for(Map.Entry<String, Integer> e1:e) {
-		System.out.println(e1.getKey());
-		
+	public static void main(String[] args) {
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("sagar", 1);
+		map.put("sidharth", 2);
+
+		Map<String, Integer> m = Collections.synchronizedMap(map);
+
+		Set<Map.Entry<String, Integer>> e = m.entrySet();
+		for (Map.Entry<String, Integer> e1 : e) {
+			System.out.println(e1.getKey());
+
+		}
 	}
-}
-	
-	
-	
+
 }
