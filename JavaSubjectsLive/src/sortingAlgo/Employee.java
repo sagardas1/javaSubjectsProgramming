3702eb07c75f2af77a89216d729361409dcf737d
sagardas1 <sagardas.195@gmail.com>
2020-09-14 16:37:25 +0530

    changes

---
+++ JavaSubjectsLive/src/sortingAlgo/Employee.java
@@ -0,0 +1,20 @@
+package sortingAlgo;
+
+public class Employee {
+
+	private String name;
+	private int redge;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRedge() {
+		return redge;
+	}
+	public void setRedge(int redge) {
+		this.redge = redge;
+	}
+	
+}
---
+++ JavaSubjectsLive/src/sortingAlgo/MainClass.java
@@ -0,0 +1,27 @@
+package sortingAlgo;
+
+import java.util.*;
+
+import com.google.gson.Gson;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.setName("vinaya");
+		employee.setRedge(1);
+		Employee employee1 = new Employee();
+		employee1.setName("Sidharth");
+		employee1.setRedge(2);
+		Employee employee2 = new Employee();
+		employee2.setName("Adu");
+		employee2.setRedge(6);
+		List<Employee> list = new ArrayList<Employee>();
+		list.add(employee);
+		list.add(employee1);
+		list.add(employee2);
+		Collections.sort(list, new MyComparator());
+
+		System.out.println(new Gson().toJson(list));
+	}
+
+}
