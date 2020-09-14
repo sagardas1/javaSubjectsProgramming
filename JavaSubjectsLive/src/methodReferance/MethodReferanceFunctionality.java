5f4ec2aa00aaac2533f81d96c53046a793d1249d
sagardas1 <sagardas.195@gmail.com>
2020-09-14 19:50:54 +0530

    changes

--- JavaSubjectsLive/src/methodReferance/MethodReferanceFunctionality.java
+++ JavaSubjectsLive/src/methodReferance/MethodReferanceFunctionality.java
@@ -2,13 +2,12 @@ package methodReferance;
 
 public class MethodReferanceFunctionality {
 
-	
 	public static void main(String[] args) {
-		MyfunctionalInterface interface1=() ->{
+		MyfunctionalInterface interface1 = () -> {
 			System.out.println("I am a boy");
 		};
-		
+
 		interface1.say();
-		
+
 	}
 }
