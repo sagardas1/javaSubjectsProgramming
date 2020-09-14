a074bcc60464d1617924ed8546e756f67876af4b
sagardas1 <sagardas.195@gmail.com>
2020-09-14 19:22:05 +0530

    changes

--- JavaSubjectsLive/src/sortingAlgo/MainClass.java
+++ JavaSubjectsLive/src/sortingAlgo/MainClass.java
@@ -9,32 +9,46 @@ public class MainClass {
 	public static void main(String[] args) {
 		Employee employee = new Employee();
 		employee.setName("vinaya");
-		employee.setRedge(1);
+		employee.setRedge(6);
 		Employee employee1 = new Employee();
 		employee1.setName("Sidharth");
 		employee1.setRedge(2);
 		Employee employee2 = new Employee();
 		employee2.setName("Adu");
-		employee2.setRedge(6);
+		employee2.setRedge(7);
 		List<Employee> list = new ArrayList<Employee>();
 		list.add(employee);
 		list.add(employee1);
 		list.add(employee2);
-		Collections.sort(list, new MyComparator());
 
-		Map<String, Employee> map = new HashMap<String, Employee>();
-		map.put(employee.getName(), employee);
-		map.put(employee1.getName(), employee1);
-		map.put(employee2.getName(), employee2);
+		Comparator<Employee> em = (emp1, emp2) -> {
+			if (emp1.getRedge() > emp2.getRedge())
+				return 1;
+			else if (emp1.getRedge() < emp2.getRedge())
+				return -1;
+			else
+				return 0;
+		};
 
-		ArrayList<Map.Entry<String, Employee>> listt = new ArrayList<Entry<String, Employee>>(map.entrySet());
-		Collections.sort(listt, new MapComparetor());
-		for (Map.Entry<String, Employee> entry : listt) {
-			System.out.print(entry.getKey() + "  ");
-			System.out.println(new Gson().toJson(entry.getValue()));
+		Collections.sort(list, em);
+
+		for (Employee e : list) {
+			System.out.println(new Gson().toJson(e));
 		}
 
-		System.out.println(new Gson().toJson(list));
+//		Map<String, Employee> map = new HashMap<String, Employee>();
+//		map.put(employee.getName(), employee);
+//		map.put(employee1.getName(), employee1);
+//		map.put(employee2.getName(), employee2);
+
+//		ArrayList<Map.Entry<String, Employee>> listt = new ArrayList<Entry<String, Employee>>(map.entrySet());
+		// Collections.sort(listt, new MapComparetor());
+//		for (Map.Entry<String, Employee> entry : listt) {
+//			System.out.print(entry.getKey() + "  ");
+//			System.out.println(new Gson().toJson(entry.getValue()));
+//		}
+
+		//System.out.println(new Gson().toJson(list));
 	}
 
 }
