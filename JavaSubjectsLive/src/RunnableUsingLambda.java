3020c3bdc06961d762a041dfb4c3eef41dab1afb
sagardas1 <sagardas.195@gmail.com>
2020-09-14 19:28:04 +0530

    changes

---
+++ JavaSubjectsLive/src/RunnableUsingLambda.java
@@ -0,0 +1,20 @@
+
+public class RunnableUsingLambda {
+
+	
+	public static void main(String[] args) {
+		
+		Runnable r=()->{
+			for(int i=0;i<10;i++) {
+				System.out.println("i am in runnable state "+i);
+			}
+		};
+		
+		Thread thread=new Thread(r);
+		thread.start();
+		
+		for(int i =0;i<10;i++) {
+			System.out.println("i am running in main method "+i );
+		}
+	}
+}
