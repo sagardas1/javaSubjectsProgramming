d01ce3bbb87a8ffdbed98f8a88e9b0ee2625b443
sagardas1 <sagardas.195@gmail.com>
2020-09-14 19:56:20 +0530

    chhnages

--- JavaSubjectsLive/src/lambdaExample/MainMethod.java
+++ JavaSubjectsLive/src/lambdaExample/MainMethod.java
@@ -1,9 +1,38 @@
 package lambdaExample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.google.gson.Gson;
+
+import sortingAlgo.Employee;
+
 public class MainMethod {
 	public static void main(String[] args) {
-		MyLambda lambda=(a,b)->a*b;
-		System.out.println(lambda.mul(5, 5));
+		
+		MyLambda l=(a,b) ->  a*b;
+		l.mul(1, 2);
+		
+		
+		
+		Employee employee=new Employee();
+		employee.setName("sahaer");
+		employee.setRedge(6);
+		Employee employee2=new Employee();
+		employee2.setName("yada");
+		employee2.setRedge(2);
+		
+		ArrayList<Employee> list=new ArrayList<Employee>();
+		list.add(employee);
+		list.add(employee2);
+		
+		
+		
+		Comparator<Employee> c=(emp,emp1) -> 0;
+		Collections.sort(list, c);
+		
+		System.out.println(new Gson().toJson(list));
 	}
 
 }
