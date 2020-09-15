ab53ac59df28832bb2c53dcfc0c9e54f7e1658d2
sagardas1 <sagardas.195@gmail.com>
2020-09-15 17:39:39 +0530

    changes

--- JavaSubjectsLive/src/doublyLinkedList/MyDoublyLinkedlList.java
+++ JavaSubjectsLive/src/doublyLinkedList/MyDoublyLinkedlList.java
@@ -1,34 +1,37 @@
 package doublyLinkedList;
-class Node{
-	
-	int data ;
+
+class Node {
+
+	int data;
 	Node previous;
 	Node next;
-	
+
 }
+
 public class MyDoublyLinkedlList {
-public static void main(String[] args) {
-	DoublyLinked doublyLinked=new DoublyLinked();
-	Node root=null;
-	root=doublyLinked.insert(root,10);
-	
+	public static void main(String[] args) {
+		DoublyLinked doublyLinked = new DoublyLinked();
+		Node root = null;
+		root = doublyLinked.insert(root, 10);
+
+	}
 }
-}
-class DoublyLinked{
+
+class DoublyLinked {
 
 	public Node insert(Node root, int i) {
-	if(root==null) {
-		return createNewNode(i);
-	}
+		if (root == null) {
+			return createNewNode(i);
+		}
 		return null;
 	}
 
 	private Node createNewNode(int i) {
-	Node node=new Node();
-	node.data=i;
-	node.previous=null;
-	node.next=null;
+		Node node = new Node();
+		node.data = i;
+		node.previous = null;
+		node.next = null;
 		return node;
 	}
-	
+
 }
