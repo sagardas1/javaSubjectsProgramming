cf23d6e352137be3f06769ebfbfa7dbc73343a78
sagardas1 <sagardas.195@gmail.com>
2020-09-15 21:11:57 +0530

    changes

--- JavaSubjectsLive/src/ListandMapSorting/SortByComparatorByroll.java
+++ JavaSubjectsLive/src/ListandMapSorting/SortByComparatorByroll.java
@@ -9,22 +9,20 @@ public class SortByComparatorByroll implements Comparator<Object> {
 	@Override
 	public int compare(Object o1, Object o2) {
 		@SuppressWarnings("unchecked")
-		Map.Entry<String, Employee> m1=(Entry<String, Employee>) o1;
+		Map.Entry<String, Employee> m1 = (Entry<String, Employee>) o1;
 		@SuppressWarnings("unchecked")
-		Map.Entry<String, Employee> m2=(Entry<String, Employee>) o2;
-		
-		Employee e1=m1.getValue();
-		Employee e2=m2.getValue();
-		
-		if(e1.getRollNo()>e2.getRollNo())return 1;
-		else if (e1.getRollNo()<e2.getRollNo()) return -1;
-		else return 0;
-		
-		
-		
-		
-		
-	
+		Map.Entry<String, Employee> m2 = (Entry<String, Employee>) o2;
+
+		Employee e1 = m1.getValue();
+		Employee e2 = m2.getValue();
+
+		if (e1.getRollNo() > e2.getRollNo())
+			return 1;
+		else if (e1.getRollNo() < e2.getRollNo())
+			return -1;
+		else
+			return 0;
+
 	}
 
 }
