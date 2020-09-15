5cf0e08ede453907b899a7dede6c93ff19918e8b
sagardas1 <sagardas.195@gmail.com>
2020-09-15 21:11:31 +0530

    changes

--- JavaSubjectsLive/src/lambdaExample/MainMethod.java
+++ JavaSubjectsLive/src/lambdaExample/MainMethod.java
@@ -10,28 +10,24 @@ import sortingAlgo.Employee;
 
 public class MainMethod {
 	public static void main(String[] args) {
-		
-		MyLambda l=(a,b) ->  a*b;
+
+		MyLambda l = (a, b) -> a * b;
 		l.mul(1, 2);
-		
-		
-		
-		Employee employee=new Employee();
+
+		Employee employee = new Employee();
 		employee.setName("sahaer");
 		employee.setRedge(6);
-		Employee employee2=new Employee();
+		Employee employee2 = new Employee();
 		employee2.setName("yada");
 		employee2.setRedge(2);
-		
-		ArrayList<Employee> list=new ArrayList<Employee>();
+
+		ArrayList<Employee> list = new ArrayList<Employee>();
 		list.add(employee);
 		list.add(employee2);
-		
-		
-		
-		Comparator<Employee> c=(emp,emp1) -> 0;
+
+		Comparator<Employee> c = (emp, emp1) -> 0;
 		Collections.sort(list, c);
-		
+
 		System.out.println(new Gson().toJson(list));
 	}
 
