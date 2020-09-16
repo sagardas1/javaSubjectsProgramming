dcba560d4f05f5e2edf25d6e7cff6e667f3e67f6
sagardas1 <sagardas.195@gmail.com>
2020-09-16 20:46:47 +0530

    changes

--- JavaSubjectsLive/src/threadPool/Service.java
+++ JavaSubjectsLive/src/threadPool/Service.java
@@ -2,9 +2,8 @@ package threadPool;
 
 public class Service {
 
-	
 	public void execute(int a) {
-		for(int i=0;i<10;i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("I am executing by new thread");
 		}
 	}
