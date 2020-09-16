d67f2ebe96463df976ce20d2bff5d5fffd41aa75
sagardas1 <sagardas.195@gmail.com>
2020-09-16 20:46:26 +0530

    changes

--- JavaSubjectsLive/src/threadPool/ThreadPoolConcept.java
+++ JavaSubjectsLive/src/threadPool/ThreadPoolConcept.java
@@ -5,22 +5,19 @@ import java.util.concurrent.Executors;
 
 public class ThreadPoolConcept {
 
-	
 	public static void main(String[] args) {
-		Service service=new Service();
-	int count=
-	Runtime.getRuntime().availableProcessors();
-	
-	System.out.println(count);
-		Runnable r=() ->{
+		Service service = new Service();
+		int count = Runtime.getRuntime().availableProcessors();
+
+		System.out.println(count);
+		Runnable r = () -> {
 			service.execute(count);
 		};
-		
-		ExecutorService executorService =Executors.newFixedThreadPool(10);
-		for(int i=0;i<10;i++) {
+
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 10; i++) {
 			executorService.execute(r);
 		}
-		
-		
+
 	}
 }
