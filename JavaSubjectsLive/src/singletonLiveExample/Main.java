8819fc1e3b097a5e307bdfc23cc38358555e7b76
sagardas1 <sagardas.195@gmail.com>
2020-09-16 20:47:49 +0530

    changes

--- JavaSubjectsLive/src/singletonLiveExample/Main.java
+++ JavaSubjectsLive/src/singletonLiveExample/Main.java
@@ -4,25 +4,22 @@ import com.google.gson.Gson;
 
 public class Main {
 	public static void main(String[] args) {
-		
-	
-	Employee employee=new Employee();
-	ContactInfo contactInfo=ContactInfo.getInstance();
-	
-	employee.seteName("Sagar");
-	employee.setRedgNo("12");
-	employee.setContactInfo(contactInfo);
 
-	
-	
-	ContactInfo contactInfo1=ContactInfo.getInstance();
-	
-	Employee employee1=new Employee();
-	
-	employee1.seteName("sidharth");
-	employee1.setRedgNo("13");
-	employee1.setContactInfo(contactInfo1);
-	System.out.println(new Gson().toJson(employee1));
+		Employee employee = new Employee();
+		ContactInfo contactInfo = ContactInfo.getInstance();
+
+		employee.seteName("Sagar");
+		employee.setRedgNo("12");
+		employee.setContactInfo(contactInfo);
+
+		ContactInfo contactInfo1 = ContactInfo.getInstance();
+
+		Employee employee1 = new Employee();
+
+		employee1.seteName("sidharth");
+		employee1.setRedgNo("13");
+		employee1.setContactInfo(contactInfo1);
+		System.out.println(new Gson().toJson(employee1));
 	}
 
 }
