9d50c858923124df595750f636a2fe6711e81fac
sagardas1 <sagardas.195@gmail.com>
2020-09-17 22:28:04 +0530

    chanegs

--- JavaSubjectsLive/src/callablePackage/CallableClass.java
+++ JavaSubjectsLive/src/callablePackage/CallableClass.java
@@ -7,17 +7,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class CallableClass {
-	
+
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		Callable<String> c= () ->{
+		Callable<String> c = () -> {
 			return "sagar das";
 		};
-		
-		ExecutorService executorService=Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-	Future<String> futureObject=	executorService.submit(c);
-	if(futureObject.isDone()) {
-		System.out.println(futureObject.get());
-	}
+
+		ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+		Future<String> futureObject = executorService.submit(c);
+		if (futureObject.isDone()) {
+			System.out.println(futureObject.get());
+		}
 	}
 
 }
