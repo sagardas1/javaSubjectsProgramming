9bff290c58b0ba4b0918306006bac7fdc3f6244e
sagardas1 <sagardas.195@gmail.com>
2020-09-18 17:34:07 +0530

    changes

--- JavaSubjectsLive/src/overload/Class.java
+++ JavaSubjectsLive/src/overload/Class.java
@@ -1,23 +1,22 @@
 package overload;
 
 public class Class {
-	
+
 	public static int m1(int a) {
-		return 10*a;
+		return 10 * a;
 	}
 
 	@SuppressWarnings("unused")
-	private static String m1(int a,int b) {
+	private static String m1(int a, int b) {
 		return "sagar";
 	}
-	
-	final  char m1(int a,int b,int c) {
+
+	final char m1(int a, int b, int c) {
 		return 'c';
 	}
-	
-	
-	public  void main(String[] args) {
+
+	public void main(String[] args) {
 		@SuppressWarnings("unused")
-		Class class1=new Class();
+		Class class1 = new Class();
 	}
 }
