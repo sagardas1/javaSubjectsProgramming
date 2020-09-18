e1705cab64734c6e39493fc97d6b21066fb658e8
sagardas1 <sagardas.195@gmail.com>
2020-09-18 17:35:18 +0530

    changes

--- JavaSubjectsLive/src/overRiddingWithException/Parent.java
+++ JavaSubjectsLive/src/overRiddingWithException/Parent.java
@@ -4,8 +4,7 @@ import java.io.FileNotFoundException;
 
 public class Parent {
 
-	
-	public void m1() throws FileNotFoundException  {
+	public void m1() throws FileNotFoundException {
 		System.out.println("this method is in Parent class");
 	}
 }
