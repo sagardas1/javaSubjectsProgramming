eb1a3b03e19c4e1ed35f60ed72b45dfe73129d46
sagardas1 <sagardas.195@gmail.com>
2020-09-18 17:40:39 +0530

    changes

--- JavaSubjectsLive/src/Serialization/SerializationExample.java
+++ JavaSubjectsLive/src/Serialization/SerializationExample.java
@@ -8,28 +8,26 @@ import java.io.ObjectOutputStream;
 
 public class SerializationExample {
 
-			
 	public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
-		
-		Employee obj=new Employee();
+
+		Employee obj = new Employee();
 		obj.setName("sagar das");
 		obj.setRollNo(12);
 		obj.setContactInfo("8908273414");
-		
-		FileOutputStream fileOutputStream=new FileOutputStream("Users/sagardas/Desktop/git");
+
+		FileOutputStream fileOutputStream = new FileOutputStream("Users/sagardas/Desktop/git");
 		@SuppressWarnings("resource")
-		ObjectOutputStream objectOutputStream=new ObjectOutputStream(fileOutputStream);
-		
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
 		objectOutputStream.writeObject(obj);
-		
-		
-		FileInputStream fileInputStream=new FileInputStream("Users/sagardas/Desktop/git");
+
+		FileInputStream fileInputStream = new FileInputStream("Users/sagardas/Desktop/git");
 		@SuppressWarnings("resource")
-		ObjectInputStream objectInputStream=new ObjectInputStream(fileInputStream);
-		
-	@SuppressWarnings("unused")
-	Employee employee=(Employee)	objectInputStream.readObject();
-		
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+		@SuppressWarnings("unused")
+		Employee employee = (Employee) objectInputStream.readObject();
+
 	}
 
 }
