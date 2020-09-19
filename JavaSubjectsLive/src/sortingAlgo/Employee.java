e41e2131eb400b8635315938d36a8d442137e146
sagardas1 <sagardas.195@gmail.com>
2020-09-19 22:01:47 +0530

    chnages

--- JavaSubjectsLive/src/sortingAlgo/Employee.java
+++ JavaSubjectsLive/src/sortingAlgo/Employee.java
@@ -4,17 +4,21 @@ public class Employee {
 
 	private String name;
 	private int redge;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getRedge() {
 		return redge;
 	}
+
 	public void setRedge(int redge) {
 		this.redge = redge;
 	}
-	
+
 }
