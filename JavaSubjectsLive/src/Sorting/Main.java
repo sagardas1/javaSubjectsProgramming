dff5a0955672d3d55b395262d8a1603669250146
sagardas1 <sagardas.195@gmail.com>
2020-09-19 22:00:21 +0530

    changes

--- JavaSubjectsLive/src/Sorting/Main.java
+++ JavaSubjectsLive/src/Sorting/Main.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Main {
 
 	public static int main(String args[]) {
-List<Object> a=new ArrayList<>();
-a.add(1);
-a.add(2);
-return 0;
+		List<Object> a = new ArrayList<>();
+		a.add(1);
+		a.add(2);
+		return 0;
 
 	}
 }
