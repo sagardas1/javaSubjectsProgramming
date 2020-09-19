ec4e4ce1ae77ee029c2b11cbaaa0e5e6ef57125b
sagardas1 <sagardas.195@gmail.com>
2020-09-19 22:05:25 +0530

    changes

--- JavaSubjectsLive/src/multiThreading/Main.java
+++ JavaSubjectsLive/src/multiThreading/Main.java
@@ -1,12 +1,12 @@
 package multiThreading;
 
 public class Main {
-public static void main(String[] args) {
-	MyThreadExample threadExample=new MyThreadExample();
-	Thread thread=new Thread(threadExample);
-	thread.start();
-	for(int i=0;i<5;i++) {
-		System.out.println("main method");
+	public static void main(String[] args) {
+		MyThreadExample threadExample = new MyThreadExample();
+		Thread thread = new Thread(threadExample);
+		thread.start();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("main method");
+		}
 	}
-}
 }
