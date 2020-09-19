479fb5b6ae0127fb44a49ec4c68734317d656a24
sagardas1 <sagardas.195@gmail.com>
2020-09-19 22:02:08 +0530

    chnages

--- JavaSubjectsLive/src/sortingAlgo/MyComparator.java
+++ JavaSubjectsLive/src/sortingAlgo/MyComparator.java
@@ -2,11 +2,11 @@ package sortingAlgo;
 
 import java.util.Comparator;
 
-public class MyComparator implements Comparator<Employee>{
+public class MyComparator implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee o1, Employee o2) {
-	
+
 		return o1.getName().compareTo(o2.getName());
 	}
 
