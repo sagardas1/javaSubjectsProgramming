26a6908a5ea2dc18c4f21b7bfe2f5d77151212ef
sagardas1 <sagardas.195@gmail.com>
2020-09-20 17:13:28 +0530

    changes

--- JavaSubjectsLive/src/clonning/ObjectCreation.java
+++ JavaSubjectsLive/src/clonning/ObjectCreation.java
@@ -1,13 +1,13 @@
 package clonning;
 
 public class ObjectCreation {
-public static void main(String[] args) throws CloneNotSupportedException {
-	Employee employee=new Employee();
-	employee.setContactInfo("8908273414");
-	employee.setName("sagar das");
-	employee.setRollNo(13);
-	
-@SuppressWarnings("unused")
-Employee employee2=	(Employee) employee.clone();
-}
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Employee employee = new Employee();
+		employee.setContactInfo("8908273414");
+		employee.setName("sagar das");
+		employee.setRollNo(13);
+
+		@SuppressWarnings("unused")
+		Employee employee2 = (Employee) employee.clone();
+	}
 }
