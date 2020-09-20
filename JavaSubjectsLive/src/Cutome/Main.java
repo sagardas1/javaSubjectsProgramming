1df58fa9940a16495b12f7a455112118e884f4d8
sagardas1 <sagardas.195@gmail.com>
2020-09-20 14:10:33 +0530

    changes

--- JavaSubjectsLive/src/Cutome/Main.java
+++ JavaSubjectsLive/src/Cutome/Main.java
@@ -1,23 +1,21 @@
 package Cutome;
 
 public class Main {
-	
-	
-public static void main(String[] args)  {
-	try {
-		div(6,0);
-	} catch (AlphaException e) {
-		System.out.println(e.getErrormsg());
-	}
-	
-	
-}
 
-public static void div(int a,int b) throws AlphaException {
-	
-if(b==0) {
-	
-	throw new AlphaException("B cant be zero");
+	public static void main(String[] args) {
+		try {
+			div(6, 0);
+		} catch (AlphaException e) {
+			System.out.println(e.getErrormsg());
+		}
+
 	}
-}
+
+	public static void div(int a, int b) throws AlphaException {
+
+		if (b == 0) {
+
+			throw new AlphaException("B cant be zero");
+		}
+	}
 }
