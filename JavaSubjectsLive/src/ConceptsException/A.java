319b14f0512ee6ef3876af7cfdc054549030f821
sagardas1 <sagardas.195@gmail.com>
2020-09-21 22:29:46 +0530

    changes

--- JavaSubjectsLive/src/ConceptsException/A.java
+++ JavaSubjectsLive/src/ConceptsException/A.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.sql.SQLClientInfoException;
 
 public class A {
-	public void m1() throws FileNotFoundException, SQLClientInfoException {}
-	
+	public void m1() throws FileNotFoundException, SQLClientInfoException {
+	}
+
 }
