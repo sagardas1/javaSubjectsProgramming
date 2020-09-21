578956dc6b53712511033da937bbc3c37523dc84
sagardas1 <sagardas.195@gmail.com>
2020-09-21 22:28:39 +0530

    changes

--- JavaSubjectsLive/src/dummypackage/Car.java
+++ JavaSubjectsLive/src/dummypackage/Car.java
@@ -3,18 +3,21 @@ package dummypackage;
 public class Car {
 	private String name;
 	private int km;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getKm() {
 		return km;
 	}
+
 	public void setKm(int km) {
 		this.km = km;
 	}
-	
 
 }
