c37e7bf5e7dac6ebf79593e3a7f87922c9da30c5
sagardas1 <sagardas.195@gmail.com>
2020-09-21 13:26:28 +0530

    changes

---
+++ JavaSubjectsLive/src/ExceptionHandling/ExceptionExample.java
@@ -0,0 +1,28 @@
+package ExceptionHandling;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		ExceptionExample example = new ExceptionExample();
+		example.mi();
+	}
+
+	public void mi() {
+
+		System.out.println("no exception");
+		System.out.println("no exception");
+		System.out.println("no exception");
+		System.out.println("no exception");
+		try {
+			if (1 / 0 == 0) {
+				System.out.println("exception happeends exception");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("after exception");
+		System.out.println("after exception");
+
+		System.out.println("after exception");
+	}
+}
