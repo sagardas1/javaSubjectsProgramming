ed978ba1ef36efc45f198110989b8d03bbddf5d6
sagardas1 <sagardas.195@gmail.com>
2020-09-22 18:40:34 +0530

    changes

--- JavaSubjectsLive/src/java8Features/MyLambda.java
+++ JavaSubjectsLive/src/java8Features/MyLambda.java
@@ -2,17 +2,16 @@ package java8Features;
 
 public class MyLambda {
 	public static void main(String[] args) {
-		
-	
-int a=10;
-int b=20;
-MyInterface c=()->a+b;
 
-System.out.println(c.m1());
-c.m1();
+		int a = 10;
+		int b = 20;
+		MyInterface c = () -> a + b;
+
+		System.out.println(c.m1());
+		c.m1();
 	}
 }
-class m1{
-	
-	
+
+class m1 {
+
 }
