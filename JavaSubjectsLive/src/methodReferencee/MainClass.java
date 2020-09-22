83ece0c24dfb365e2003225d3f35beebc51f6ee6
sagardas1 <sagardas.195@gmail.com>
2020-09-22 13:48:55 +0530

    chnages

--- JavaSubjectsLive/src/methodReferencee/MainClass.java
+++ JavaSubjectsLive/src/methodReferencee/MainClass.java
@@ -1,11 +1,30 @@
 package methodReferencee;
 
+import java.util.*;
+
 public class MainClass {
 	
 	public static void main(String[] args) {
 		MethodReferernce methodReferernce=new MethodReferernce();
 	Sayble s=	methodReferernce:: m1;
 	System.out.println(s.say(1, 2));
+	
+	
+	
+	Map<String,Integer> a=new HashMap<>();
+	
+	
+	List<Map.Entry<String, Integer>> list=new ArrayList<>(a.entrySet());
+
+	
+	Comparator<Map.Entry<String, Integer>> comp=(o1,o2)->{
+		if(o1.getValue()>o2.getValue() ) return 1;
+		else if(o1.getValue()<o2.getValue() ) return -1;
+		else return 0;
+		
+	};
+	
+	Collections.sort(list,comp);
 	}
 
 }
