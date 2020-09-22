aeee19b53fed3c488cc2abff14b50fdcf97377c9
sagardas1 <sagardas.195@gmail.com>
2020-09-22 19:12:15 +0530

    changes

--- JavaSubjectsLive/src/ConceptsException/Main.java
+++ JavaSubjectsLive/src/ConceptsException/Main.java
@@ -4,20 +4,16 @@ import java.io.FileNotFoundException;
 import java.sql.SQLClientInfoException;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-	A aa =new A();
-	try {
-		aa.m1();
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLClientInfoException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+
+		A aa = new A();
+		try {
+			aa.m1();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLClientInfoException e) {
+			e.printStackTrace();
 		}
 	}
-
-
+}
