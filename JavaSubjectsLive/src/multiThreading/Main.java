79ddb7849145359a336dd7087f0d3a60a7cda62c
sagardas1 <sagardas.195@gmail.com>
2020-09-23 22:59:44 +0530

    changes

--- JavaSubjectsLive/src/multiThreading/Main.java
+++ JavaSubjectsLive/src/multiThreading/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		MyThreadExample threadExample = new MyThreadExample();
 		Thread thread = new Thread(threadExample);
 		thread.start();
+		
+		
 		for (int i = 0; i < 5; i++) {
 			System.out.println("main method");
 		}
