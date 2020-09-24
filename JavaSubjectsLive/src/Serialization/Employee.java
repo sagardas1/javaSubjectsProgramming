025b56310a7f8bc3979c24b7b79af28dacd331b5
sagardas1 <sagardas.195@gmail.com>
2020-09-24 19:31:23 +0530

    chnages

--- JavaSubjectsLive/src/Serialization/Employee.java
+++ JavaSubjectsLive/src/Serialization/Employee.java
@@ -3,28 +3,34 @@ package Serialization;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Employee implements Serializable{
-	
+public class Employee implements Serializable {
+
 	private String name;
 	private int rollNo;
 	private String contactInfo;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getRollNo() {
 		return rollNo;
 	}
+
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
+
 	public String getContactInfo() {
 		return contactInfo;
 	}
+
 	public void setContactInfo(String contactInfo) {
 		this.contactInfo = contactInfo;
 	}
-	
-	}
+
+}
