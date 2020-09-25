f012f4f2b45fd6dccc62b0e83c270cd0252abbf1
sagardas1 <sagardas.195@gmail.com>
2020-09-25 21:42:56 +0530

    chnages

--- JavaSubjectsLive/src/clonnable/MakingCloneable.java
+++ JavaSubjectsLive/src/clonnable/MakingCloneable.java
@@ -4,6 +4,7 @@ public class MakingCloneable {
 	public static void main(String[] args) throws CloneNotSupportedException {
 		
 		Student student=new Student("sagar", 1);
+		@SuppressWarnings("unused")
 		Student s=(Student) student.clone();
 	}
 
