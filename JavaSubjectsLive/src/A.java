08fe189d62ef75dc90a9a03dc04b203964fcc0e0
sagardas1 <sagardas.195@gmail.com>
2020-09-27 19:45:28 +0530

    changes

--- JavaSubjectsLive/src/A.java
+++ JavaSubjectsLive/src/A.java
@@ -5,6 +5,7 @@ public class A {
 
 	public static void main(String[] args) {
 		int a[] = { 1, 2, 3, 4, 5, 6, 7 };
+		@SuppressWarnings({ "rawtypes", "unused" })
 		List list = Arrays.asList(a);
 
 	}
