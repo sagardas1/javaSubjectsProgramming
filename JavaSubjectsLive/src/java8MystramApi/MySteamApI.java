42b06105c45b2f8d4737c10c148f6e0ea9a5c0ad
sagardas1 <sagardas.195@gmail.com>
2020-09-29 23:34:40 +0530

    changes

--- JavaSubjectsLive/src/java8MystramApi/MySteamApI.java
+++ JavaSubjectsLive/src/java8MystramApi/MySteamApI.java
@@ -9,12 +9,13 @@ import java.util.stream.Collectors;
 public class MySteamApI {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+	
 		List<Product> productsList = new ArrayList<Product>();
 		productsList.add(new Product(1, "HP Laptop", 25000f));
 		productsList.add(new Product(2, "Dell Laptop", 30000f));
 		productsList.add(new Product(3, "Lenevo Laptop", 28000f));
 		productsList.add(new Product(4, "Sony Laptop", 28000f));
-		
+
 		productsList.add(new Product(5, "Apple Laptop", 90000f));
 		productsList.add(new Product(5, "Apple Laptop", 90000f));
 
