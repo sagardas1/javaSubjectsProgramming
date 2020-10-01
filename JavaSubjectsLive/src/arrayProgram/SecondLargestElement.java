d04a96cff9051bb76d7d1ec125c829f304d86132
sagardas1 <sagardas.195@gmail.com>
2020-10-01 23:06:14 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/SecondLargestElement.java
+++ JavaSubjectsLive/src/arrayProgram/SecondLargestElement.java
@@ -10,8 +10,6 @@ public class SecondLargestElement {
 		for (int i = 0; i < a.length; i++) {
 			if (smallest == -1) {
 
-				
-				
 				smallest = a[i];
 			} else if (smallest >= a[i]) {
 				smallest = a[i];
