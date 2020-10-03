32f55695dd8d8de3c5c6895509adbc1ae96c1b2b
sagardas1 <sagardas.195@gmail.com>
2020-10-03 23:23:28 +0530

    changes

--- JavaSubjectsLive/src/runPack/InvestigationsService.java
+++ JavaSubjectsLive/src/runPack/InvestigationsService.java
@@ -6,6 +6,8 @@ public class InvestigationsService {
 
 	public int getMap(Map<String, Integer> map) {
 		System.out.println("i am in InvestigationsService");
+		
+		
 		return map.get("sagar");
 	}
 }
