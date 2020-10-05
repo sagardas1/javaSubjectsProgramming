b1cb809e3d8f81b417f1a6e64dde9126c875b114
sagardas1 <sagardas.195@gmail.com>
2020-10-05 21:17:38 +0530

    changes

--- JavaSubjectsLive/src/dummypackage/ComapatorByKm.java
+++ JavaSubjectsLive/src/dummypackage/ComapatorByKm.java
@@ -14,6 +14,7 @@ public class ComapatorByKm implements Comparator<Object> {
 		@SuppressWarnings("unchecked")
 		Map.Entry<String, Car> map2 = (Entry<String, Car>) o2;
 		Car car1 = map2.getValue();
+		
 
 		if (car.getKm() > car1.getKm()) {
 			return 1;
