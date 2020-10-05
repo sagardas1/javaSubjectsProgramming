52db66b5dea2c3491464b18249b832b547dfb43f
sagardas1 <sagardas.195@gmail.com>
2020-10-05 21:16:53 +0530

    changes

--- JavaSubjectsLive/src/comparetor/AgeComparetor.java
+++ JavaSubjectsLive/src/comparetor/AgeComparetor.java
@@ -10,6 +10,7 @@ public class AgeComparetor implements Comparator<Object> {
 		Employee employee = (Employee) o1;
 		Employee employee1 = (Employee) o2;
 
+		
 		if (employee.getRoll() > employee1.getRoll()) {
 			return -1;
 		} else if (employee.getRoll() < employee1.getRoll()) {
