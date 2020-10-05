a82182c7a67e2dcdb204efb7c757ada21ed383bd
sagardas1 <sagardas.195@gmail.com>
2020-10-05 21:15:22 +0530

    changes

--- JavaSubjectsLive/src/nullCheckInObject/Animal.java
+++ JavaSubjectsLive/src/nullCheckInObject/Animal.java
@@ -1,17 +1,17 @@
 package nullCheckInObject;
 
 public class Animal {
-	
+
 	@SuppressWarnings("null")
 	public static void main(String[] args) {
-		Animal animal=null;
-		if(animal==null) {
+		Animal animal = null;
+		if (animal == null) {
 			System.out.println("errror");
 		}
-		if(animal.equals(null)) {
+		if (animal.equals(null)) {
 			System.out.println("hsbhsg");
 		}
-		
+
 	}
 
 }
