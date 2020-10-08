93360a63020bd7693c3f82173b19bfb0f817e0e0
sagardas1 <sagardas.195@gmail.com>
2020-10-08 22:18:42 +0530

    changes

--- JavaSubjectsLive/src/arrayProgram/A.java
+++ JavaSubjectsLive/src/arrayProgram/A.java
@@ -7,22 +7,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class A {
-	
-	public static void main(String[] args) {
-		
-		 List<List<Integer>> list = new ArrayList<List<Integer>>();
-		 list.add(Arrays.asList(1, 2, 3));
-		 list.add(Arrays.asList(1, 2, 4));
-		 list.add(Arrays.asList(1, 2));
-		
-		 
-		 
-		 Set<Integer> sum = list.stream().map(p -> p.stream().mapToInt(i ->i).sum()).collect(Collectors.toSet());
-		 
-		 System.out.println(sum.stream().mapToInt(j ->j).sum());
-		
-	}
 
-	
+	public static void main(String[] args) {
+
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		list.add(Arrays.asList(1, 2, 3));
+		list.add(Arrays.asList(1, 2, 4));
+		list.add(Arrays.asList(1, 2));
+
+		Set<Integer> sum = list.stream().map(p -> p.stream().mapToInt(i -> i).sum()).collect(Collectors.toSet());
+
+		System.out.println(sum.stream().mapToInt(j -> j).sum());
+
+	}
 
 }
