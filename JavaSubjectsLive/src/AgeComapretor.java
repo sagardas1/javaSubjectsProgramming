d798747cbc3a0847551741af7ab125e7763fdec3
sagardas1 <sagardas.195@gmail.com>
2020-10-09 20:22:38 +0530

    changes

--- JavaSubjectsLive/src/AgeComapretor.java
+++ JavaSubjectsLive/src/AgeComapretor.java
@@ -9,7 +9,6 @@ public class AgeComapretor implements Comparator<Map.Entry<String, Student>> {
 	@Override
 	public int compare(Entry<String, Student> o1, Entry<String, Student> o2) {
 
-		
 		Student s1 = o1.getValue();
 		Student s2 = o2.getValue();
 
@@ -17,19 +16,10 @@ public class AgeComapretor implements Comparator<Map.Entry<String, Student>> {
 			return 1;
 		else if (s1.getRollNo() < s2.getRollNo())
 			return -1;
-		
+
 		else
-			
+
 			return 0;
 	}
 
-	
-	
-	
-	
-	
-	
 }
-
-
-
