3e5fe02e4d02b301ed28a083cfa76fe1e2888e73
sagardas1 <sagardas.195@gmail.com>
2020-10-09 20:24:21 +0530

    changes

--- JavaSubjectsLive/src/threadPool/ThreadPoolConcept.java
+++ JavaSubjectsLive/src/threadPool/ThreadPoolConcept.java
@@ -13,6 +13,7 @@ public class ThreadPoolConcept {
 		Runnable r = () -> {
 			service.execute(count);
 		};
+		
 
 		ExecutorService executorService = Executors.newFixedThreadPool(10);
 		for (int i = 0; i < 10; i++) {
