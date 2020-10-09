7d766b851b59e3cef60926f50afc0ee46d47ca6d
sagardas1 <sagardas.195@gmail.com>
2020-10-09 20:22:52 +0530

    changes

--- JavaSubjectsLive/src/dynamicLoading/Test2.java
+++ JavaSubjectsLive/src/dynamicLoading/Test2.java
@@ -11,7 +11,6 @@ public class Test2 {
 			Class aClass = classLoader.loadClass("dynamicLoading.Bike");
 			System.out.println("aClass.getName() = " + aClass.getName());
 
-			
 			Bike bike = (Bike) aClass.newInstance();
 			bike.ride();
 
