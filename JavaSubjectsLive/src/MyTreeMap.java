296844e0752f2d3fd2f730bb19986b8bba9a1d25
sagardas1 <sagardas.195@gmail.com>
2020-10-12 20:31:07 +0530

    changes

--- JavaSubjectsLive/src/MyTreeMap.java
+++ JavaSubjectsLive/src/MyTreeMap.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 public class MyTreeMap {
-	
-	
+
 	public static void main(String[] args) {
-		TreeMap< Integer, String> map=new TreeMap<Integer, String>();
+		TreeMap<Integer, String> map = new TreeMap<Integer, String>();
 		map.put(10, "ten");
 		map.put(4, "ten");
 		map.put(5, "ten");
@@ -12,13 +11,13 @@ public class MyTreeMap {
 		map.put(2, "ten");
 		map.put(0, "ten");
 		map.put(7, "ten");
-		
-		Set<Map.Entry<Integer,String>> set=new HashSet<Map.Entry<Integer,String>>(map.entrySet());
-		
-		for(Map.Entry<Integer,String> en:set) {
+
+		Set<Map.Entry<Integer, String>> set = new HashSet<Map.Entry<Integer, String>>(map.entrySet());
+
+		for (Map.Entry<Integer, String> en : set) {
 			System.out.println(en.getKey());
 		}
-		
+
 	}
 
 }
