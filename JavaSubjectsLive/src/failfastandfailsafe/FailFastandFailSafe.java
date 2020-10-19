4291f830c47a42b8b023e2eeb3178481178f38b5
sagardas1 <sagardas.195@gmail.com>
2020-10-19 16:26:25 +0530

    changes

--- JavaSubjectsLive/src/failfastandfailsafe/FailFastandFailSafe.java
+++ JavaSubjectsLive/src/failfastandfailsafe/FailFastandFailSafe.java
@@ -10,6 +10,7 @@ public class FailFastandFailSafe {
 		List<String> a = new ArrayList<String>();
 		a.add("Sagar das");
 		a.add("sidharth das");
+		
 
 		for (@SuppressWarnings("unused")
 		String s : a) {
