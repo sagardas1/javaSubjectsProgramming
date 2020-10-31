d9bc4041894bb82ca536a3d1af082b800fd1aa90
sagardas1 <sagardas.195@gmail.com>
2020-10-31 20:05:11 +0530

    entrySet

--- JavaSubjectsLive/src/RunnableUsingLambda.java
+++ JavaSubjectsLive/src/RunnableUsingLambda.java
@@ -3,6 +3,7 @@ public class RunnableUsingLambda {
 
 	public static void main(String[] args) {
 
+		
 		Runnable r = () -> {
 			for (int i = 0; i < 10; i++) {
 				System.out.println("i am in runnable state " + i);
