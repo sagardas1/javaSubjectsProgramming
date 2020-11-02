bf365bfd4fdd716d16c8ddb6c0ffdd0bb6584615
sagardas1 <sagardas.195@gmail.com>
2020-11-02 13:09:16 +0530

    changes

---
+++ JavaSubjectsLive/src/Example1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.google.gson.Gson;
+
+public class Example1 {
+public static void main(String[] args) {
+	ArrayList<Integer> al = new ArrayList<>(); 
+    al.add(1); 
+    al.add(2); 
+    al.add(3); 
+    al.add(4); 
+    al.add(5); 
+
+    Iterator<Integer> itr = al.iterator(); 
+    while (itr.hasNext()) { 
+        if (itr.next() == 2) { 
+            // will not throw Exception 
+            itr.remove(); 
+            al.add(20);
+        } 
+    } 
+}
+}
