77a496c2a7cd4248763148ad10e8a14c1e188446
sagardas1 <sagardas.195@gmail.com>
2020-11-07 14:43:27 +0530

    changes

--- JavaSubjectsLive/src/Anaragram.java
+++ JavaSubjectsLive/src/Anaragram.java
@@ -1,33 +1,30 @@
 
 public class Anaragram {
 	public static void main(String[] args) {
-		String s1="sagar";
-		String s2="agars";
-		int size=s1.length();
-		
-		
-		if(s1.length()==s2.length()) {
-			
-			
-			while(size!=0) {
-				char a=s1.charAt(0);
-				
-			s1=	s1.replace(a+"", "");
-				
-			s2=	s2.replace(a+"", "");
-			size=size-(size-s1.length());
-				
-				
-			
-				
+		String s1 = "sagar";
+		String s2 = "agars";
+		int size = s1.length();
+
+		if (s1.length() == s2.length()) {
+
+			while (size != 0) {
+				char a = s1.charAt(0);
+
+				s1 = s1.replace(a + "", "");
+
+				s2 = s2.replace(a + "", "");
+				size = size - (size - s1.length());
+
 			}
-			if(s1.length()+s2.length()==0) {
+			if (s1.length() + s2.length() == 0) {
 				System.out.println(true);
-				
+
 			}
-			
-		}else {System.out.println(false);}
-		
+
+		} else {
+			System.out.println(false);
+		}
+
 	}
 
 }
