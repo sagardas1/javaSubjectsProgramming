290c9e40153ff4cae6427961d4438f7a0cfc9380
sagardas1 <sagardas.195@gmail.com>
2020-11-08 19:38:51 +0530

    changes

--- JavaSubjectsLive/src/FibonacciSeries.java
+++ JavaSubjectsLive/src/FibonacciSeries.java
@@ -1,36 +1,38 @@
 
 public class FibonacciSeries {
-	static int count=0;
-static	int a;
-static	int b;
+	static int count = 0;
+	static int a;
+	static int b;
+
 	public static void main(String[] args) {
-		
-		int numbers=7;
-		
-		fibona(numbers-2);
-		
+
+		int numbers = 7;
+
+		fibona(numbers - 2);
+
 	}
 
 	private static void fibona(int i) {
-		
-		if(count==0) {
+
+		if (count == 0) {
 			System.out.println(0);
-			
-			count=1;
-			a=0;b=1;
-		}else {
-			int temp=a;
-			a=b;
-			b=temp+a;
-			i=i-1;
-			
+
+			count = 1;
+			a = 0;
+			b = 1;
+		} else {
+			int temp = a;
+			a = b;
+			b = temp + a;
+			i = i - 1;
+
 		}
-	
+
 		System.out.println(b);
-		if(i!=0) {
+		if (i != 0) {
 			fibona(i);
 		}
-		
+
 	}
 
 }
