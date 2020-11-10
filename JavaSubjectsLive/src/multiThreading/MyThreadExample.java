15a7779159042851dfbfbe8d7b949303be061751
sagardas1 <sagardas.195@gmail.com>
2020-11-10 19:21:57 +0530

    changes

--- JavaSubjectsLive/src/multiThreading/MyThreadExample.java
+++ JavaSubjectsLive/src/multiThreading/MyThreadExample.java
@@ -3,9 +3,7 @@ package multiThreading;
 public class MyThreadExample implements Runnable {
 	public void run() {
 		for (int i = 0; i < 5; i++) {
-			
-			
-			
+
 			System.out.println("run Method");
 		}
 
